9c7502367ce0e7b60294001eca3c97761df10463
Ettore Perazzoli <ettore@src.gnome.org>
2003-08-15 17:32:08 +0000

    Ooops, fix copyright notices.

--- libedataserver/e-source-list.c
+++ libedataserver/e-source-list.c
@@ -1,7 +1,7 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 8; tab-width: 8 -*- */
 /* e-source-list.c
  *
- * Copyright (C) 2003  Ettore Perazzoli
+ * Copyright (C) 2003  Ximian, Inc.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of version 2 of the GNU General Public
