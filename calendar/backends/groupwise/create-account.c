363c713bd9908c9fa3ff01edd89c13b24ef81247
Rodrigo Moya <rodrigo@ximian.com>
2003-12-04 13:22:36 +0000

    added simple program to add accounts to the calendar sources until we a
    2003-12-04  Rodrigo Moya <rodrigo@ximian.com>
    
    	* backends/groupwise/create-account.c: added simple program to
    	add accounts to the calendar sources until we a GUI to configure
    	them.
    
    	* backends/groupwise/Makefile.am: added noinst program.

---
+++ calendar/backends/groupwise/create-account.c
@@ -0,0 +1,47 @@
+
+#include <config.h>
+#include <gconf/gconf-client.h>
+#include <libgnome/gnome-init.h>
+#include <libedataserver/e-source-list.h>
+
+static GConfClient *conf_client;
+
+static void
+add_account (const char *conf_key, const char *hostname)
+{
+	ESourceList *source_list;
+	ESourceGroup *group;
+	ESource *source;
+
+	source_list = e_source_list_new_for_gconf (conf_client, conf_key);
+	group = e_source_group_new (hostname, "groupwise://");
+	source = e_source_new ("Default", hostname);
+	e_source_set_group (source, group);
+
+	e_source_list_add_group (source_list, group, -1);
+}
+
+int
+main (int argc, char *argv[])
+{
+	gnome_program_init (PACKAGE, VERSION,
+			    LIBGNOME_MODULE,
+			    argc, argv,
+			    NULL);
+
+	if (argc != 2) {
+		g_print ("Usage: %s hostname\n", argv[0]);
+		return -1;
+	}
+
+	/* initialize variables */
+	conf_client = gconf_client_get_default ();
+
+	add_account ("/apps/evolution/calendar/sources", argv[1]);
+	add_account ("/apps/evolution/tasks/sources", argv[1]);
+
+	/* terminate */
+	g_object_unref (conf_client);
+
+	return 0;
+}
