391d2965e2e6ed481231119b33a5d4902d353616
Rodrigo Moya <rodrigo@ximian.com>
2003-12-10 16:21:32 +0000

    added support for specifying a password in the command line.
    2003-12-10  Rodrigo Moya <rodrigo@ximian.com>
    
    	* backends/groupwise/soap-test.c: added support for specifying a
    	password in the command line.

--- calendar/backends/groupwise/soap-test.c
+++ calendar/backends/groupwise/soap-test.c
@@ -4,14 +4,14 @@
 #include "e-gw-connection.h"
 
 static GMainLoop *main_loop;
-static char *arg_hostname, *arg_username;
+static char *arg_hostname, *arg_username, *arg_password;
 
 static gboolean
 idle_cb (gpointer data)
 {
 	EGwConnection *cnc;
 
-	cnc = e_gw_connection_new (arg_hostname, arg_username, NULL);
+	cnc = e_gw_connection_new (arg_hostname, arg_username, arg_password);
 	if (E_IS_GW_CONNECTION (cnc)) {
 		g_print ("Connected to %s!\n", arg_hostname);
 
@@ -32,13 +32,17 @@ main (int argc, char *argv[])
 			    argc, argv,
 			    NULL);
 
-	if (argc != 3) {
-		g_print ("Usage: %s hostname username\n", argv[0]);
+	if (argc != 3 && argc != 4) {
+		g_print ("Usage: %s hostname username [password]\n", argv[0]);
 		return -1;
 	}
 
 	arg_hostname = argv[1];
 	arg_username = argv[2];
+	if (argc == 4)
+		arg_password = argv[3];
+	else
+		arg_password = NULL;
 
 	main_loop = g_main_loop_new (NULL, TRUE);
 	g_idle_add ((GSourceFunc) idle_cb, NULL);
