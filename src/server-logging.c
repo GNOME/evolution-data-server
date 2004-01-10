0ac69713ec1e3e6fcc2548fd08db2daf99464e88
Rodrigo Moya <rodrigo@src.gnome.org>
2004-01-10 22:22:45 +0000

    Reverted my previous commit

--- src/server-logging.c
+++ src/server-logging.c
@@ -103,7 +103,6 @@ server_log_handler(const gchar *domain,
 
 	CORBA_free (log_event.domain);
 	CORBA_free (log_event.message);
-	bonobo_arg_release (value);
 
 	g_log_default_handler (domain, flags, msg, NULL);
 }
