25a32129e82e41fd3e9b9251744df98700d9a4bb
Chris Toshok <toshok@ximian.com>
2004-03-12 22:10:23 +0000

    test setting CATEGORIES as well as CATEGORY_LIST.
    2004-03-12  Chris Toshok  <toshok@ximian.com>
    
    	* tests/ebook/test-categories.c (main): test setting CATEGORIES as
    	well as CATEGORY_LIST.

--- addressbook/tests/ebook/test-categories.c
+++ addressbook/tests/ebook/test-categories.c
@@ -9,7 +9,7 @@ int
 main (int argc, char **argv)
 {
 	EContact *contact;
-	GList *category_list, *l;
+	GList *category_list;
 	char *categories;
 
 	gnome_program_init("test-categories", "0.0", LIBGNOME_MODULE, argc, argv, NULL);
@@ -17,6 +17,7 @@ main (int argc, char **argv)
 	if (bonobo_init (&argc, argv) == FALSE)
 		g_error ("Could not initialize Bonobo");
 
+	printf ("--- Testing setting CATEGORY_LIST\n");
 	contact = e_contact_new ();
 
 	category_list = NULL;
@@ -27,18 +28,35 @@ main (int argc, char **argv)
 	e_contact_set (contact, E_CONTACT_CATEGORY_LIST, category_list);
 
 	category_list = e_contact_get (contact, E_CONTACT_CATEGORY_LIST);
-	for (l = category_list; l; l = l->next) {
-	  printf ("category: %s\n", (char*)l->data);
-	}
 
 	printf ("vcard = \n%s\n", e_vcard_to_string (E_VCARD (contact), EVC_FORMAT_VCARD_30));
 
 	categories = e_contact_get (contact, E_CONTACT_CATEGORIES);
 
 	if (strcmp (categories, "Birthday,Business,Competition"))
-	  printf ("failed\n");
+	  printf ("failed conversion from list to string\n");
 	else
-	  printf ("passed\n");
-	
+	  printf ("passed conversion from list to string\n");
+
+	g_object_unref (contact);
+
+	printf ("--- Testing setting CATEGORIES\n");
+	contact = e_contact_new ();
+
+	e_contact_set (contact, E_CONTACT_CATEGORIES, "Birthday,Business,Competition");
+
+	printf ("vcard = \n%s\n", e_vcard_to_string (E_VCARD (contact), EVC_FORMAT_VCARD_30));
+
+	category_list = e_contact_get (contact, E_CONTACT_CATEGORY_LIST);
+	if (g_list_length (category_list) != 3)
+	  printf ("failed conversion from string to list\n");
+	else {
+	  if (!strcmp ("Birthday", (char*)g_list_nth_data (category_list, 0)) &&
+	      !strcmp ("Business", (char*)g_list_nth_data (category_list, 1)) &&
+	      !strcmp ("Competition", (char*)g_list_nth_data (category_list, 2)))
+	    printf ("passed conversion from string to list\n");
+	  else
+	    printf ("failed conversion from string to list\n");
+	}
 	return 0;
 }
