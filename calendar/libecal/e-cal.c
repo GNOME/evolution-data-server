36cd046494de805976d90ce054aa3b9b008b5141
JP Rosevear <jpr@novell.com>
2004-10-06 16:45:47 +0000

    Fixes #67513
    2004-10-06  JP Rosevear  <jpr@novell.com>
    
     	Fixes #67513
    
     	* libecal/e-cal.c (cal_objects_sent_cb): deep copy the user list
     	data

--- calendar/libecal/e-cal.c
+++ calendar/libecal/e-cal.c
@@ -632,6 +632,7 @@ cal_objects_sent_cb (ECalListener *listener, ECalendarStatus status, GList *user
 {
 	ECal *ecal = data;
 	ECalendarOp *op;
+	GList *l;
 
 	op = e_calendar_get_op (ecal);
 
@@ -645,6 +646,9 @@ cal_objects_sent_cb (ECalListener *listener, ECalendarStatus status, GList *user
 	op->status = status;
 	op->list = g_list_copy (users);
 	op->string = g_strdup (object);
+
+	for (l = op->list; l; l = l->next)
+		l->data = g_strdup (l->data);
 
 	g_cond_signal (op->cond);
 
