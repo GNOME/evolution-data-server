0bed1fc091c8870d7810b16c8ddbba3f13c8dc67
Jeffrey Stedfast <fejj@novell.com>
2004-12-09 18:09:26 +0000

    Need to have the folder as the first vararg.
    2004-12-07  Jeffrey Stedfast  <fejj@novell.com>
    
    	* providers/imap4/camel-imap4-folder.c (imap4_append_message):
    	Need to have the folder as the first vararg.

--- camel/providers/imap4/camel-imap4-folder.c
+++ camel/providers/imap4/camel-imap4-folder.c
@@ -929,9 +929,9 @@ imap4_append_message (CamelFolder *folder, CamelMimeMessage *message,
  retry:
 	
 	if (engine->capa & CAMEL_IMAP4_CAPABILITY_UIDPLUS)
-		ic = camel_imap4_engine_queue (engine, NULL, "UID APPEND %F%s%s %L\r\n", flags, date, message);
+		ic = camel_imap4_engine_queue (engine, NULL, "UID APPEND %F%s%s %L\r\n", folder, flags, date, message);
 	else
-		ic = camel_imap4_engine_queue (engine, NULL, "APPEND %F%s%s %L\r\n", flags, date, message);
+		ic = camel_imap4_engine_queue (engine, NULL, "APPEND %F%s%s %L\r\n", folder, flags, date, message);
 	
 	while ((id = camel_imap4_engine_iterate (engine)) < ic->id && id != -1)
 		;
