97392b16ba48b836c09e00aa62263233bd39d34c
Sushma Rai <rsushma@src.gnome.org>
2005-07-25 09:04:04 +0000

    Handling gcc specific "__PRETTY_FUNCTION__" macro for non-gcc compilers. Fix for #271837, by "bugzilla-novell@thewrittenword.com (The Written Word)".

--- addressbook/libebook/e-book.c
+++ addressbook/libebook/e-book.c
@@ -25,6 +25,7 @@ GMainContext *_ebook_context;
 
 static GObjectClass *parent_class;
 
+#ifdef __PRETTY_FUNCTION__
 #define e_return_error_if_fail(expr,error_code)	G_STMT_START{		\
      if G_LIKELY(expr) { } else						\
        {								\
@@ -43,6 +44,24 @@ static GObjectClass *parent_class;
 		#expr);							\
 	 return FALSE;							\
        };				}G_STMT_END
+#else
+#define e_return_error_if_fail(expr,error_code)	G_STMT_START{		\
+     if G_LIKELY(expr) { } else						\
+       {								\
+	 g_log (G_LOG_DOMAIN,						\
+		G_LOG_LEVEL_CRITICAL,					\
+		"file %s: line %d: assertion `%s' failed",		\
+		__FILE__,						\
+		__LINE__,						\
+		#expr);							\
+	 g_set_error (error, E_BOOK_ERROR, (error_code),                \
+		"file %s: line %d: assertion `%s' failed",		\
+		__FILE__,						\
+		__LINE__,						\
+		#expr);							\
+	 return FALSE;							\
+       };				}G_STMT_END
+#endif
 
 /* XXX we need a better error message here */
 #define E_BOOK_CHECK_STATUS(status,error) G_STMT_START{			\
