983146398ced27622845698f8c74c5403c3b0ffd
Parthasarathi Susarla <sparthasarathi@novell.com>
2005-08-10 12:23:32 +0000

    return E_GW_CONNECTION_OTHER for error 53530
    2005-08-10  Parthasarathi Susarla <sparthasarathi@novell.com>
    
    	* e-gw-connection.c: (e_gw_connection_parse_response_status):
    	return E_GW_CONNECTION_OTHER for error 53530

--- servers/groupwise/e-gw-connection.c
+++ servers/groupwise/e-gw-connection.c
@@ -142,7 +142,7 @@ e_gw_connection_parse_response_status (SoupSoapResponse *response)
 	case 59914: return E_GW_CONNECTION_STATUS_ITEM_ALREADY_ACCEPTED;
 	case 59910: return E_GW_CONNECTION_STATUS_INVALID_CONNECTION;
 	case 59923: return E_GW_CONNECTION_STATUS_REDIRECT;
-	case 53530: return E_GW_CONNECTION_OTHER;
+	case 53530: return E_GW_CONNECTION_STATUS_OTHER;
 		/* FIXME: map all error codes */
 	}
 
