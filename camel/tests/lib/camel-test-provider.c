d620f5485989e5b88a3abdc283b573183367496d
Tor Lillqvist <tml@novell.com>
2005-09-15 22:33:20 +0000

    Use G_MODULE_SUFFIX instead of hardcoded .so.
    2005-09-16  Tor Lillqvist  <tml@novell.com>
    
    	* lib/camel-test-provider.c (camel_test_provider_init): Use
    	G_MODULE_SUFFIX instead of hardcoded .so.

--- camel/tests/lib/camel-test-provider.c
+++ camel/tests/lib/camel-test-provider.c
@@ -12,7 +12,7 @@ void camel_test_provider_init(int argc, char **argv)
 	CamelException ex = { 0 };
 
 	for (i=0;i<argc;i++) {
-		name = g_strdup_printf("libcamel%s.so", argv[i]);
+		name = g_strdup_printf("libcamel%s."G_MODULE_SUFFIX, argv[i]);
 		path = g_build_filename(CAMEL_BUILD_DIR, "providers", argv[i], ".libs", name, NULL);
 		g_free(name);
 		camel_provider_load(path, &ex);
