8e94f36881e06ee23c7348d5fdb55cf7585abf03
Parthasarathi Susarla <sparthasarathi@novell.com>
2006-02-16 09:36:28 +0000

    proceed only if diary is a valid pointer
    2006-02-16  Parthasarathi Susarla <sparthasarathi@novell.com>
    
    	* camel-disco-diary.c: (camel_disco_diary_log):
    	proceed only if diary is a valid pointer

--- camel/camel-disco-diary.c
+++ camel/camel-disco-diary.c
@@ -126,7 +126,7 @@ camel_disco_diary_log (CamelDiscoDiary *diary, CamelDiscoDiaryAction action,
 	d(printf("diary log: %s\n", diary->file?"ok":"no file!"));
 
 	/* You may already be a loser. */
-	if (!diary->file)
+	if (!diary && !diary->file)
 		return;
 
 	status = camel_file_util_encode_uint32 (diary->file, action);
