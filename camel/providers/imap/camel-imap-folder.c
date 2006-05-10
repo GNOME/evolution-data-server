7bb42747715e60f018d652ffa4adda3224e1af1d
Parthasarathi Susarla <sparthasarathi@novell.com>
2006-05-10 08:49:18 +0000

    Add space at the end of CAMEL_MESSAGE_INFO_HEADERS. The content-type
    2006-05-10  Parthasarathi Susarla <sparthasarathi@novell.com>
    
    	* camel-imap-folder.c:
    	Add space at the end of CAMEL_MESSAGE_INFO_HEADERS. The content-type
    	header wont be fetched otherwise(it mergers with the
    	MESSAGE_LIST_HEADER).
    	This fix shows the attachment icon in the message list

--- camel/providers/imap/camel-imap-folder.c
+++ camel/providers/imap/camel-imap-folder.c
@@ -2322,7 +2322,7 @@ add_message_from_data (CamelFolder *folder, GPtrArray *messages,
 }
 
 
-#define CAMEL_MESSAGE_INFO_HEADERS "DATE FROM TO CC SUBJECT REFERENCES IN-REPLY-TO MESSAGE-ID MIME-VERSION CONTENT-TYPE"
+#define CAMEL_MESSAGE_INFO_HEADERS "DATE FROM TO CC SUBJECT REFERENCES IN-REPLY-TO MESSAGE-ID MIME-VERSION CONTENT-TYPE "
 
 /* FIXME: this needs to be kept in sync with camel-mime-utils.c's list
    of mailing-list headers and so might be best if this were
