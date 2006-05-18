d366da0703351b61222b947cd98f635594d1b45a
Jeffrey Stedfast <fejj@src.gnome.org>
2006-05-18 15:28:10 +0000

    updated the (c) years

--- camel/providers/imap4/camel-imap4-specials.c
+++ camel/providers/imap4/camel-imap4-specials.c
@@ -1,6 +1,6 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 /*  Camel
- *  Copyright (C) 1999-2004 Jeffrey Stedfast
+ *  Copyright (C) 1999-2006 Jeffrey Stedfast
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
