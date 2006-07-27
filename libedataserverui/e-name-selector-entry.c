f5e8d8f627421aaa5012a7386ef333def695da35
Devashish Sharma <dsharma@src.gnome.org>
2006-07-27 10:51:48 +0000

    Fix for Bug 347706 and 347779

--- libedataserverui/e-name-selector-entry.c
+++ libedataserverui/e-name-selector-entry.c
@@ -1569,8 +1569,10 @@ destination_row_changed (ENameSelectorEntry *name_selector_entry, GtkTreePath *p
 	n = gtk_tree_path_get_indices (path) [0];
 	destination = e_destination_store_get_destination (name_selector_entry->destination_store, iter);
 
+	if (!destination) 
+		return;
+	
 	g_assert (n >= 0);
-	g_assert (destination != NULL);
 
 	entry_text = gtk_entry_get_text (GTK_ENTRY (name_selector_entry));
 	if (!get_range_by_index (entry_text, n, &range_start, &range_end)) {
@@ -1818,8 +1820,9 @@ editor_closed_cb (GtkObject *editor, gpointer data)
 	EBook *book;
 	gboolean result;
 	gint email_num;
-	ENameSelectorEntry *name_selector_entry = E_NAME_SELECTOR_ENTRY (data);
 	
+	ENameSelectorEntry *name_selector_entry = E_NAME_SELECTOR_ENTRY (data);
+
 	destination = name_selector_entry->popup_destination;
 	contact = e_destination_get_contact (destination);
 	if (!contact)
@@ -1845,6 +1848,7 @@ editor_closed_cb (GtkObject *editor, gpointer data)
 	g_free (contact_uid);
 	g_object_unref (contact);
 	g_object_unref (editor);
+	g_object_unref (name_selector_entry);
 }
 
 static void
@@ -1925,6 +1929,7 @@ popup_activate_contact (ENameSelectorEntry *name_selector_entry, GtkWidget *menu
 			return;
 
 		contact_editor = (*name_selector_entry->contact_editor_func) (book, contact, FALSE, TRUE);
+		g_object_ref (name_selector_entry);
 		g_signal_connect (contact_editor, "editor_closed",
 				  G_CALLBACK (editor_closed_cb), name_selector_entry);
 	}
