b54dabd30021bfcc8cd6b4915cd3a6892c27480e
Pascal Terjan <pterjan@linuxfr.org>
2007-05-27 22:44:59 +0000

    The parameter remember_type was renamed to type 3 years ago but not in the
    2007-05-28  Pascal Terjan  <pterjan@linuxfr.org>
    
           * e-passwords.c (e_passwords_ask_password):
           The parameter remember_type was renamed to type 3 years ago but not in the doc
    
    
    svn path=/trunk/; revision=7786

--- libedataserverui/e-passwords.c
+++ libedataserverui/e-passwords.c
@@ -1129,7 +1129,7 @@ e_passwords_add_password (const char *key, const char *passwd)
  * the password (e.g. Mail, Addressbook, etc.)
  * @key: key to store the password under
  * @prompt: prompt string
- * @remember_type: whether or not to offer to remember the password,
+ * @type: whether or not to offer to remember the password,
  * and for how long.
  * @remember: on input, the default state of the remember checkbox.
  * on output, the state of the checkbox when the dialog was closed.
