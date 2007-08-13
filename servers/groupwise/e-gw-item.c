c11f37d19550558a22e9b1c479cd59c13f34baf6
Suman Manjunath <msuman@novell.com>
2007-08-13 06:31:56 +0000

    ** Fixes bug #465419
    2007-08-10  Suman Manjunath  <msuman@novell.com>
    
    	** Fixes bug #465419
    
    	* e-gw-item.c: (e_gw_item_append_to_soap_message):
    	If message body is NULL, use 0 as string length.
    
    
    svn path=/trunk/; revision=7942

--- servers/groupwise/e-gw-item.c
+++ servers/groupwise/e-gw-item.c
@@ -3187,7 +3187,7 @@ e_gw_item_append_to_soap_message (EGwItem *item, SoupSoapMessage *msg)
 
 			str = soup_base64_encode (priv->message, strlen (priv->message));
 			//str = g_strdup (priv->message);
-			str_len = g_strdup_printf ("%d", (int)strlen (str));
+			str_len = g_strdup_printf ("%d", str ? (int)strlen (str) : 0);
 			soup_soap_message_start_element (msg, "part", NULL, NULL);
 			soup_soap_message_add_attribute (msg, "length", str_len, NULL, NULL);
 			soup_soap_message_add_attribute (msg, "contentType", priv->content_type, NULL, NULL);
