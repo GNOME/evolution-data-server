747477ad4e6851f75a92daecad782aec61473a44
Matthew Barnes <mbarnes@redhat.com>
2007-11-12 00:57:51 +0000

    Disable SSLv2 compatible HELLO on SSL stream when SSLv2 is disabled
    2007-11-11  Matthew Barnes  <mbarnes@redhat.com>
    
    	* camel/camel-tcp-stream-ssl.c (enable_ssl):
    	Disable SSLv2 compatible HELLO on SSL stream when SSLv2 is
    	disabled (#345135, patch from Niels Vorgaard Christensen).
    
    
    svn path=/trunk/; revision=8192

--- camel/camel-tcp-stream-ssl.c
+++ camel/camel-tcp-stream-ssl.c
@@ -1041,10 +1041,13 @@ enable_ssl (CamelTcpStreamSSL *ssl, PRFileDesc *fd)
 		return NULL;
 	
 	SSL_OptionSet (ssl_fd, SSL_SECURITY, PR_TRUE);
-	if (ssl->priv->flags & CAMEL_TCP_STREAM_SSL_ENABLE_SSL2)
+	if (ssl->priv->flags & CAMEL_TCP_STREAM_SSL_ENABLE_SSL2) {
 		SSL_OptionSet (ssl_fd, SSL_ENABLE_SSL2, PR_TRUE);
-	else
+		SSL_OptionSet (ssl_fd, SSL_V2_COMPATIBLE_HELLO, PR_TRUE);
+	} else {
 		SSL_OptionSet (ssl_fd, SSL_ENABLE_SSL2, PR_FALSE);
+		SSL_OptionSet (ssl_fd, SSL_V2_COMPATIBLE_HELLO, PR_FALSE);
+	}
 	if (ssl->priv->flags & CAMEL_TCP_STREAM_SSL_ENABLE_SSL3)
 		SSL_OptionSet (ssl_fd, SSL_ENABLE_SSL3, PR_TRUE);
 	else
