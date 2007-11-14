d5076f30f5f56ad480c8227415b3ca02b750268b
Johnny Jacob <jjohnny@src.gnome.org>
2007-11-14 18:22:45 +0000

    Missed files in previous commit
    svn path=/branches/EXCHANGE_MAPI_BRANCH/; revision=8208

---
+++ calendar/backends/mapi/e-cal-backend-mapi-utils.h
@@ -0,0 +1,37 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ *  Suman Manjunath <msuman@novell.com>
+ *  Copyright (C) 2007 Novell, Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+
+#ifndef E_CAL_BACKEND_MAPI_UTILS_H
+#define E_CAL_BACKEND_MAPI_UTILS_H
+#endif
+
+#include <e-cal-backend-mapi.h>
+#include <libecal/e-cal-component.h>
+
+void
+e_cal_backend_mapi_props_to_comp (struct mapi_SPropValue_array *properties, ECalComponent *comp, const icaltimezone *default_zone);
+
+gboolean
+build_name_id (struct mapi_nameid *nameid, gpointer data);
+
+int
+build_props (struct SPropValue **value, struct SPropTagArray *SPropTagArray, gpointer data);
+
