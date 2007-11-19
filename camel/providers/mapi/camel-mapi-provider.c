e1e6401ed03cead757b6e453d9dd3cde195ee4a3
Srinivasa Ragavan <sragavan@novell.com>
2007-11-19 06:46:19 +0000

    Added support for additional configuration.
    2007-11-19  Srinivasa Ragavan  <sragavan@novell.com>
    
            * camel-mapi-provider.c: (camel_provider_module_init): Added support
    	for additional configuration.
    
    
    svn path=/branches/EXCHANGE_MAPI_BRANCH/; revision=8223

--- camel/providers/mapi/camel-mapi-provider.c
+++ camel/providers/mapi/camel-mapi-provider.c
@@ -45,6 +45,47 @@ static guint mapi_url_hash (gconstpointer);
 static gint check_equal (char *, char *);
 static gint mapi_url_equal (gconstpointer, gconstpointer);
 
+static CamelProviderConfEntry mapi_conf_entries[] = {
+	{ CAMEL_PROVIDER_CONF_SECTION_START, "mailcheck", NULL,
+	  N_("Checking for New Mail") },
+	{ CAMEL_PROVIDER_CONF_CHECKBOX, "check_all", NULL,
+	  N_("C_heck for new messages in all folders"), "1" },
+	{ CAMEL_PROVIDER_CONF_SECTION_END },
+
+/* 	/\* override the labels/defaults of the standard settings *\/ */
+/* 	{ CAMEL_PROVIDER_CONF_LABEL, "username", NULL, */
+/* 	  /\* i18n: the '_' should appear before the same letter it */
+/* 	     does in the evolution:mail-config.glade "User_name" */
+/* 	     translation (or not at all) *\/ */
+/* 	  N_("Windows User_name:") }, */
+
+	/* extra Exchange configuration settings */
+	{ CAMEL_PROVIDER_CONF_SECTION_START, "activedirectory", NULL,
+	  /* i18n: GAL is an Outlookism, AD is a Windowsism */
+	  N_("Global Address List / Active Directory") },
+	{ CAMEL_PROVIDER_CONF_ENTRY, "ad_server", NULL,
+	  /* i18n: "Global Catalog" is a Windowsism, but it's a
+	     technical term and may not have translations? */
+	  N_("_Global Catalog server name:") },
+	{ CAMEL_PROVIDER_CONF_CHECKSPIN, "ad_limit", NULL,
+	  N_("_Limit number of GAL responses: %s"), "y:1:500:10000" },
+	{ CAMEL_PROVIDER_CONF_SECTION_END },
+	{ CAMEL_PROVIDER_CONF_SECTION_START, "generals", NULL,
+	  N_("Options") },
+	{ CAMEL_PROVIDER_CONF_CHECKBOX, "sync_offline", NULL,
+	  N_("Automatically synchroni_ze account locally"), "0" },
+	{ CAMEL_PROVIDER_CONF_CHECKBOX, "filter", NULL,
+	  /* i18n: copy from evolution:camel-imap-provider.c */
+	  N_("_Apply filters to new messages in Inbox on this server"), "0" },
+	{ CAMEL_PROVIDER_CONF_CHECKBOX, "filter_junk", NULL,
+	  N_("Check new messages for _Junk contents"), "0" },
+	{ CAMEL_PROVIDER_CONF_CHECKBOX, "filter_junk_inbox", "filter_junk",
+	  N_("Only check for Junk messag_es in the Inbox folder"), "0" },
+
+	 	
+	{ CAMEL_PROVIDER_CONF_SECTION_END },
+	{ CAMEL_PROVIDER_CONF_END }
+};
 
 static CamelProvider mapi_provider = {
 	"mapi",	
@@ -56,11 +97,11 @@ static CamelProvider mapi_provider = {
 	"mail",	
 
 	CAMEL_PROVIDER_IS_REMOTE | CAMEL_PROVIDER_IS_SOURCE |
-	CAMEL_PROVIDER_IS_STORAGE | CAMEL_PROVIDER_DISABLE_SENT_FOLDER, 
+	CAMEL_PROVIDER_IS_STORAGE | CAMEL_PROVIDER_DISABLE_SENT_FOLDER | CAMEL_PROVIDER_IS_EXTERNAL, 
 
 	CAMEL_URL_NEED_USER | CAMEL_URL_NEED_HOST,
 
-	0,
+	mapi_conf_entries,
 
 	/* ... */
 };
@@ -96,7 +137,6 @@ void
 camel_provider_module_init(void)
 {
 	mapi_provider.name = "Exchange MAPI";
-	mapi_provider.extra_conf = NULL;
 	mapi_provider.auto_detect = mapi_auto_detect_cb;
 	mapi_provider.authtypes = g_list_prepend (mapi_provider.authtypes, &camel_mapi_password_authtype);
 	mapi_provider.url_hash = mapi_url_hash;
