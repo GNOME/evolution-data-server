1e710e1eab90ad49199e5c7aee731d7b03e4f695
Jeffrey Stedfast <fejj@novell.com>
2007-12-26 02:06:16 +0000

    Fixes bug #224026
    2007-12-25  Jeffrey Stedfast  <fejj@novell.com>
    
    	Fixes bug #224026
    
    	* camel-charset-map-private.h: Regenerated.
    
    	* camel-charset-map.c (main): Rewritten to be in-sync with GMime's
    	implementation which has support for multibyte charsets and
    	Arabic.
    
    
    svn path=/trunk/; revision=8319

--- camel/camel-charset-map.c
+++ camel/camel-charset-map.c
@@ -27,14 +27,21 @@
 #include <config.h>
 #endif
 
-#include <errno.h>
+#include <glib.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
+#ifdef HAVE_CODESET
+#include <langinfo.h>
+#endif
+#include <iconv.h>
+#include <errno.h>
 
 /*
   if you want to build the charset map, compile this with something like:
-    gcc -DBUILD_MAP camel-charset-map.c `glib-config --cflags`
+    gcc -DBUILD_MAP camel-charset-map.c `pkg-config --cflags --libs glib-2.0`
   (plus any -I/-L/-l flags you need for iconv), then run it as
     ./a.out > camel-charset-map-private.h
 
@@ -48,12 +55,11 @@
 */
 
 #ifdef BUILD_MAP
-#include <iconv.h>
-#include <glib.h>
 
 static struct {
-	char *name;
-	unsigned int bit;	/* assigned bit */
+	char *name;        /* charset name */
+	int multibyte;     /* charset type */
+	unsigned int bit;  /* assigned bit */
 } tables[] = {
 	/* These are the 8bit character sets (other than iso-8859-1,
 	 * which is special-cased) which are supported by both other
@@ -61,20 +67,35 @@ static struct {
 	 * they're listed in is the order they'll be tried in, so put
 	 * the more-popular ones first.
 	 */
-	{ "iso-8859-2", 0 },	/* Central/Eastern European */
-	{ "iso-8859-4", 0 },	/* Baltic */
-	{ "koi8-r", 0 },	/* Russian */
-	{ "koi8-u", 0 },	/* Ukranian */
-	{ "iso-8859-5", 0 },	/* Least-popular Russian encoding */
-	{ "iso-8859-7", 0 },	/* Greek */
-	{ "iso-8859-8", 0 },    /* Hebrew; Visual */
-	{ "iso-8859-9", 0 },	/* Turkish */
-	{ "iso-8859-13", 0 },	/* Baltic again */
-	{ "iso-8859-15", 0 },	/* New-and-improved iso-8859-1, but most
-				 * programs that support this support UTF8
-				 */
-	{ "windows-1251", 0 },	/* Russian */
-	{ 0, 0 }
+	{ "iso-8859-2",   0, 0 },  /* Central/Eastern European */
+	{ "iso-8859-4",   0, 0 },  /* Baltic */
+	{ "koi8-r",       0, 0 },  /* Russian */
+	{ "koi8-u",       0, 0 },  /* Ukranian */
+	{ "iso-8859-5",   0, 0 },  /* Least-popular Russian encoding */
+	{ "iso-8859-6",   0, 0 },  /* Arabic */
+	{ "iso-8859-7",   0, 0 },  /* Greek */
+	{ "iso-8859-8",   0, 0 },  /* Hebrew; Visual */
+	{ "iso-8859-9",   0, 0 },  /* Turkish */
+	{ "iso-8859-13",  0, 0 },  /* Baltic again */
+	{ "iso-8859-15",  0, 0 },  /* New-and-improved iso-8859-1, but most
+				    * programs that support this support UTF8
+				    */
+	{ "windows-1251", 0, 0 },  /* Russian */
+	
+	/* These are the multibyte character sets which are commonly
+	 * supported by other mail clients. Note: order for multibyte
+	 * charsets does not affect priority unlike the 8bit charsets
+	 * listed above.
+	 */
+	{ "iso-2022-jp",  1, 0 },  /* Japanese designed for use over the Net */
+	{ "Shift-JIS",    1, 0 },  /* Japanese as used by Windows and MacOS systems */
+	{ "euc-jp",       1, 0 },  /* Japanese traditionally used on Unix systems */
+	{ "euc-kr",       1, 0 },  /* Korean */
+	{ "iso-2022-kr",  1, 0 },  /* Korean (less popular than euc-kr) */
+	{ "gb2312",       1, 0 },  /* Simplified Chinese */
+	{ "Big5",         1, 0 },  /* Traditional Chinese */
+	{ "euc-tw",       1, 0 },
+	{ NULL,           0, 0 }
 };
 
 unsigned int encoding_map[256 * 256];
@@ -85,128 +106,200 @@ unsigned int encoding_map[256 * 256];
 #define UCS "UCS-4LE"
 #endif
 
-int main (void)
+static guint
+block_hash (gconstpointer v)
 {
-	int i, j;
-	int max, min;
-	int bit = 0x01;
-	int k;
+	const signed char *p = v;
+	guint32 h = *p++;
+	int i;
+	
+	for (i = 0; i < 256; i++)
+		h = (h << 5) - h + *p++;
+	
+	return h;
+}
+
+static int
+block_equal (gconstpointer v1, gconstpointer v2)
+{
+	return !memcmp (v1, v2, 256);
+}
+
+int main (int argc, char **argv)
+{
+	unsigned char *block = NULL;
+	unsigned int bit = 0x01;
+	GHashTable *table_hash;
+	size_t inleft, outleft;
+	char *inbuf, *outbuf;
+	guint32 out[128], c;
+	char in[128];
+	int i, j, k;
 	int bytes;
 	iconv_t cd;
-	char in[128];
-	guint32 out[128];
-	char *inptr, *outptr;
-	size_t inlen, outlen;
-
+	
 	/* dont count the terminator */
-	bytes = ((sizeof(tables)/sizeof(tables[0]))+7-1)/8;
-
+	bytes = ((sizeof (tables) / sizeof (tables[0])) + 7 - 1) / 8;
+	g_assert (bytes <= 4);
+	
 	for (i = 0; i < 128; i++)
 		in[i] = i + 128;
-
-	for (j = 0; tables[j].name; j++) {
+	
+	for (j = 0; tables[j].name && !tables[j].multibyte; j++) {
 		cd = iconv_open (UCS, tables[j].name);
-		if (cd == (iconv_t)-1)
-			exit (1);
-		inptr = in;
-		outptr = (char *)(out);
-		inlen = sizeof (in);
-		outlen = sizeof (out);
-		while (iconv (cd, &inptr, &inlen, &outptr, &outlen) == -1) {
+		inbuf = in;
+		inleft = sizeof (in);
+		outbuf = (char *) out;
+		outleft = sizeof (out);
+		while (iconv (cd, &inbuf, &inleft, &outbuf, &outleft) == -1) {
 			if (errno == EILSEQ) {
-				inptr++;
-				inlen--;
+				inbuf++;
+				inleft--;
 			} else {
-				printf ("%s\n", strerror (errno));
+				g_warning ("iconv (%s->UCS4, ..., %d, ..., %d): %s",
+					   tables[j].name, inleft, outleft,
+					   g_strerror (errno));
 				exit (1);
 			}
 		}
 		iconv_close (cd);
-
-		for (i = 0; i < 128 - outlen / 4; i++) {
+		
+		for (i = 0; i < 128 - outleft / 4; i++) {
 			encoding_map[i] |= bit;
 			encoding_map[out[i]] |= bit;
 		}
-
+		
 		tables[j].bit = bit;
 		bit <<= 1;
 	}
-
-	printf("/* This file is automatically generated: DO NOT EDIT */\n\n");
-
-	for (i=0;i<256;i++) {
-		/* first, do we need this block? */
-		for (k=0;k<bytes;k++) {
-			for (j=0;j<256;j++) {
-				if ((encoding_map[i*256 + j] & (0xff << (k*8))) != 0)
-					break;
-			}
-			if (j < 256) {
-				/* yes, dump it */
-				printf("static const unsigned char m%02x%x[256] = {\n\t", i, k);
-				for (j=0;j<256;j++) {
-					printf("0x%02x, ", (encoding_map[i*256+j] >> (k*8)) & 0xff );
-					if (((j+1)&7) == 0 && j<255)
-						printf("\n\t");
-				}
-				printf("\n};\n\n");
-			}
-		}
-	}
-
-	printf("static const struct {\n");
-	for (k=0;k<bytes;k++) {
-		printf("\tconst unsigned char *bits%d;\n", k);
-	}
-	printf("} camel_charmap[256] = {\n\t");
-	for (i=0;i<256;i++) {
-		/* first, do we need this block? */
-		printf("{ ");
-		for (k=0;k<bytes;k++) {
-			for (j=0;j<256;j++) {
-				if ((encoding_map[i*256 + j] & (0xff << (k*8))) != 0)
-					break;
-			}
-			if (j < 256) {
-				printf("m%02x%x, ", i, k);
+	
+	/* Mutibyte tables */
+	for ( ; tables[j].name && tables[j].multibyte; j++) {
+		cd = iconv_open (tables[j].name, UCS);
+		if (cd == (iconv_t) -1)
+			continue;
+		
+		for (c = 128, i = 0; c < 65535 && i < 65535; c++) {
+			inbuf = (char *) &c;
+			inleft = sizeof (c);
+			outbuf = in;
+			outleft = sizeof (in);
+			
+			if (iconv (cd, &inbuf, &inleft, &outbuf, &outleft) != (size_t) -1) {
+				/* this is a legal character in charset table[j].name */
+				iconv (cd, NULL, NULL, &outbuf, &outleft);
+				encoding_map[i++] |= bit;
+				encoding_map[c] |= bit;
 			} else {
-				printf("NULL, ");
+				/* reset the iconv descriptor */
+				iconv (cd, NULL, NULL, NULL, NULL);
 			}
 		}
-		printf("}, ");
-		if (((i+1)&7) == 0 && i<255)
-			printf("\n\t");
+		
+		iconv_close (cd);
+		
+		tables[j].bit = bit;
+		bit <<= 1;
 	}
-	printf("\n};\n\n");
-
-	printf("static const struct {\n\tconst char *name;\n\tunsigned int bit;\n} camel_charinfo[] = {\n");
-	for (j=0;tables[j].name;j++) {
-		printf("\t{ \"%s\", 0x%04x },\n", tables[j].name, tables[j].bit);
+	
+	printf ("/* This file is automatically generated: DO NOT EDIT */\n\n");
+	
+	table_hash = g_hash_table_new_full (block_hash, block_equal, g_free, g_free);
+	
+	for (i = 0; i < 256; i++) {
+		for (k = 0; k < bytes; k++) {
+			char name[32], *alias;
+			int has_bits = FALSE;
+			
+			if (!block) {
+				/* we reuse malloc'd blocks that are not added to the
+				 * hash table to avoid unnecessary malloc/free's */
+				block = g_malloc (256);
+			}
+			
+			for (j = 0; j < 256; j++) {
+				if ((block[j] = (encoding_map[i * 256 + j] >> (k * 8)) & 0xff))
+					has_bits = TRUE;
+			}
+			
+			if (!has_bits)
+				continue;
+			
+			sprintf (name, "m%02x%x", i, k);
+			
+			if ((alias = g_hash_table_lookup (table_hash, block))) {
+				/* this block is identical to an earlier block, just alias it */
+				printf ("#define %s %s\n\n", name, alias);
+			} else {
+				/* unique block, dump it */
+				g_hash_table_insert (table_hash, block, g_strdup (name));
+				
+				printf ("static unsigned char %s[256] = {\n\t", name);
+				for (j = 0; j < 256; j++) {
+					printf ("0x%02x, ", block[j]);
+					if (((j + 1) & 7) == 0 && j < 255)
+						printf ("\n\t");
+				}
+				printf ("\n};\n\n");
+				
+				/* force the next loop to malloc a new block */
+				block = NULL;
+			}
+		}
 	}
-	printf("};\n\n");
-
-	printf("#define charset_mask(x) \\\n");
-	for (k=0;k<bytes;k++) {
-		if (k!=0)
-			printf("\t| ");
+	
+	g_hash_table_destroy (table_hash);
+	g_free (block);
+	
+	printf ("static const struct {\n");
+	for (k = 0; k < bytes; k++)
+		printf ("\tconst unsigned char *bits%d;\n", k);
+	
+	printf ("} camel_charmap[256] = {\n\t");
+	for (i = 0; i < 256; i++) {
+		printf ("{ ");
+		for (k = 0; k < bytes; k++) {
+			for (j = 0; j < 256; j++) {
+				if ((encoding_map[i * 256 + j] & (0xff << (k * 8))) != 0)
+					break;
+			}
+			
+			if (j < 256)
+				printf ("m%02x%x, ", i, k);
+			else
+				printf ("NULL, ");
+		}
+		
+		printf ("}, ");
+		if (((i + 1) & 3) == 0 && i < 255)
+			printf ("\n\t");
+	}
+	printf ("\n};\n\n");
+	
+	printf ("struct {\n\tconst char *name;\n\tunsigned int bit;\n} camel_charinfo[] = {\n");
+	for (j = 0; tables[j].name; j++)
+		printf ("\t{ \"%s\", 0x%08x },\n", tables[j].name, tables[j].bit);
+	printf ("};\n\n");
+	
+	printf ("#define charset_mask(x) \\\n");
+	for (k = 0; k < bytes; k++) {
+		if (k != 0)
+			printf ("\t| ");
 		else
-			printf("\t");
-		printf("(camel_charmap[(x)>>8].bits%d?camel_charmap[(x)>>8].bits%d[(x)&0xff]<<%d:0)", k, k, k*8);
-		if (k<bytes-1)
-			printf("\t\\\n");
+			printf ("\t");
+		
+		printf ("(camel_charmap[(x) >> 8].bits%d ? camel_charmap[(x) >> 8].bits%d[(x) & 0xff] << %d : 0)",
+			k, k, k * 8);
+		
+		if (k < bytes - 1)
+			printf ("\t\\\n");
 	}
-	printf("\n\n");
-
+	printf ("\n\n");
+	
 	return 0;
 }
 
 #else
-
-#include <glib.h>
-#include <locale.h>
-#ifdef HAVE_CODESET
-#include <langinfo.h>
-#endif
 
 #include "camel-charset-map.h"
 #include "camel-charset-map-private.h"
