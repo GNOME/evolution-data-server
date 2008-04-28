0d2c1d93a8a7c9ed78374c826b0d3d3c7d241fb2
Sebastien Bacher <seb128@ubuntu.com>
2008-04-28 19:29:38 +0000

    - don't free the same variable twice (#530323)
    2008-04-28  Sebastien Bacher  <seb128@ubuntu.com>
    
    	* backends/contacts/e-cal-backend-contacts.c: (book_record_new):
    	- don't free the same variable twice (#530323)
    
    svn path=/branches/gnome-2-22/; revision=8691

--- calendar/backends/contacts/e-cal-backend-contacts.c
+++ calendar/backends/contacts/e-cal-backend-contacts.c
@@ -102,7 +102,6 @@ book_record_new (ECalBackendContacts *cbc, ESource *source)
         query = e_book_query_any_field_contains ("");
 
         if (!e_book_get_book_view (book, query, fields, -1, &book_view, NULL)) {
-		g_list_free (fields);
                 e_book_query_unref (query);
                 g_object_unref (book);
                 g_list_free (fields);
