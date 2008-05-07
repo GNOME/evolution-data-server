6aa05f1edb8babf17a5ae8b207d4a5b21139ca17
Jeffrey Stedfast <fejj@src.gnome.org>
2008-05-07 03:17:01 +0000

    fixed header ordering
    svn path=/trunk/; revision=8764

--- camel/camel-gpg-context.c
+++ camel/camel-gpg-context.c
@@ -30,17 +30,17 @@
 #include <config.h>
 #endif
 
-#include <ctype.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/time.h>
-#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <ctype.h>
 
 #include <glib.h>
 #include <glib/gi18n-lib.h>
