fdb4d97832a09215beab7fa84dee86d69dc63055
Srinivasa Ragavan <sragavan@novell.com>
2008-08-04 04:09:08 +0000

    Disable debug crashers.
    2008-08-04  Srinivasa Ragavan  <sragavan@novell.com>
    
    	* camel/camel-string-utils.c: Disable debug crashers.
    
    svn path=/trunk/; revision=9253

--- camel/camel-string-utils.c
+++ camel/camel-string-utils.c
@@ -276,7 +276,7 @@ camel_pstring_free(const char *s)
 		if (count == 0) {
 			g_hash_table_remove(pstring_table, p);
 			g_free(p);
-			if (!g_getenv("CDS_DEBUG")) { 
+			if (g_getenv("CDS_DEBUG")) { 
 				if (p != s) /* Only for debugging purposes */
 					g_assert(0);
 			}
@@ -284,7 +284,7 @@ camel_pstring_free(const char *s)
 			g_hash_table_insert(pstring_table, p, GINT_TO_POINTER(count));
 		}
 	} else {
-		if (!g_getenv("CDS_DEBUG")) {
+		if (g_getenv("CDS_DEBUG")) {
 			g_warning("Trying to free string not allocated from the pool '%s'", s);
 			/*Only for debugging purposes */
 			g_assert (0);
