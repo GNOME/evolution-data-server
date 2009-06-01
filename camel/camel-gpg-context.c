814cea20fc405e282b8c3be6f9f17bf81d904f5e
Milan Crha <mcrha@redhat.com>
2009-06-01 18:59:40 +0200

    Bug #580212 - Report errors in CamelException when verifying signature

--- camel/camel-gpg-context.c
+++ camel/camel-gpg-context.c
@@ -1559,7 +1559,16 @@ gpg_verify (CamelCipherContext *context, CamelMimePart *ipart, CamelException *e
 			goto exception;
 	}
 
-	gpg_ctx_op_wait (gpg);
+	if (gpg_ctx_op_wait (gpg) != 0) {
+		const gchar *diagnostics;
+
+		diagnostics = gpg_ctx_get_diagnostics (gpg);
+		camel_exception_set (ex, CAMEL_EXCEPTION_SYSTEM,
+				     diagnostics && *diagnostics ? diagnostics :
+				     _("Failed to execute gpg."));
+		goto exception;
+	}
+
 	validity = camel_cipher_validity_new ();
 	diagnostics = gpg_ctx_get_diagnostics (gpg);
 	camel_cipher_validity_set_description (validity, diagnostics);
