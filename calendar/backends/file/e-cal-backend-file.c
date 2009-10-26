8ce49da1e073e3cdce4355adee40d0348bc2f100
Milan Crha <mcrha@redhat.com>
2009-10-26 12:52:03 +0100

    Compiler warning fix

--- calendar/backends/file/e-cal-backend-file.c
+++ calendar/backends/file/e-cal-backend-file.c
@@ -2529,6 +2529,7 @@ remove_instance (ECalBackendFile *cbfile, ECalBackendFileObject *obj_data, const
 {
 	gchar *hash_rid;
 	ECalComponent *comp;
+	struct icaltimetype current;
 
 	if (!rid || !*rid)
 		return;
@@ -2554,8 +2555,8 @@ remove_instance (ECalBackendFile *cbfile, ECalBackendFileObject *obj_data, const
 				     icaltime_from_string (rid), CALOBJ_MOD_THIS);
 
 	/* Since we are only removing one instance of recurrence
-       event, update the last modified time on the component */
-	struct icaltimetype current = icaltime_current_time_with_zone (icaltimezone_get_utc_timezone ());
+	   event, update the last modified time on the component */
+	current = icaltime_current_time_with_zone (icaltimezone_get_utc_timezone ());
 	e_cal_component_set_last_modified (obj_data->full_object, &current);
 
 	/* add the modified object to the beginning of the list,
