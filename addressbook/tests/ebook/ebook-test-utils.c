401dda4b3848f107c26fc12da6e6062ea0430c90
Travis Reitter <treitter@gmail.com>
2009-12-03 16:54:35 -0800

    Add test program for the e-book 'getContact' method.

--- addressbook/tests/ebook/ebook-test-utils.c
+++ addressbook/tests/ebook/ebook-test-utils.c
@@ -1,9 +1,90 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 
 #include <stdlib.h>
+#include <glib.h>
 #include <libebook/e-book.h>
 
 #include "ebook-test-utils.h"
+
+char*
+ebook_test_utils_book_add_contact (EBook    *book,
+                                   EContact *contact)
+{
+        GError *error = NULL;
+
+        if (!e_book_add_contact (book, contact, &error)) {
+                const char *uri;
+
+                uri = e_book_get_uri (book);
+                g_warning ("failed to add contact to addressbook: `%s': %s",
+                                uri, error->message);
+                exit(1);
+        }
+
+        return e_contact_get (contact, E_CONTACT_UID);
+}
+
+EContact*
+ebook_test_utils_book_get_contact (EBook      *book,
+                                   const char *uid)
+{
+        EContact *contact = NULL;
+        GError *error = NULL;
+
+        if (!e_book_get_contact (book, uid, &contact, &error)) {
+                const char *uri;
+
+                uri = e_book_get_uri (book);
+                g_warning ("failed to get contact '%s' in addressbook: `%s': "
+                                "%s", uid, uri, error->message);
+                exit(1);
+        }
+
+        return contact;
+}
+
+static void
+get_contact_cb (EBook            *book,
+                EBookStatus       status,
+                EContact         *contact,
+                EBookTestClosure *closure)
+{
+        const char *uid;
+
+        if (status != E_BOOK_ERROR_OK) {
+                g_warning ("failed to asynchronously get the contact: "
+                                "status %d", status);
+                exit (1);
+        }                       
+
+        uid = e_contact_get_const (contact, E_CONTACT_UID);
+        g_print ("successfully asynchronously retrieved the contact '%s'\n",
+                        uid);
+
+        if (closure) {
+                (*closure->cb) (closure->user_data);
+                g_free (closure);
+        }
+}
+
+void   
+ebook_test_utils_book_async_get_contact (EBook       *book,
+                                         const char  *uid,
+                                         GSourceFunc  callback,
+                                         gpointer     user_data)
+{       
+        EBookTestClosure *closure;
+        
+        closure = g_new0 (EBookTestClosure, 1);
+        closure->cb = callback;
+        closure->user_data = user_data;
+        if (e_book_async_get_contact (book, uid,
+                                (EBookContactCallback) get_contact_cb,
+                                closure)) {
+                g_warning ("failed to set up async getContact");
+                exit(1);
+        }
+}
 
 EBook*
 ebook_test_utils_book_new_temp (char **uri)
@@ -11,13 +92,14 @@ ebook_test_utils_book_new_temp (char **uri)
         EBook *book;
 	GError *error = NULL;
 	gchar *file_template;
+        char *uri_result;
 
         file_template = g_build_filename (g_get_tmp_dir (),
                         "ebook-test-XXXXXX/", NULL);
 	g_mkstemp (file_template);
 
-	*uri = g_filename_to_uri (file_template, NULL, &error);
-	if (!*uri) {
+	uri_result = g_filename_to_uri (file_template, NULL, &error);
+	if (!uri_result) {
                 g_warning ("failed to convert %s to an URI: %s", file_template,
                                 error->message);
 		exit (1);
@@ -26,12 +108,17 @@ ebook_test_utils_book_new_temp (char **uri)
 
 	/* create a temp addressbook in /tmp */
 	g_print ("loading addressbook\n");
-	book = e_book_new_from_uri (*uri, &error);
+	book = e_book_new_from_uri (uri_result, &error);
 	if (!book) {
                 g_warning ("failed to create addressbook: `%s': %s", *uri,
                                 error->message);
 		exit(1);
 	}
+
+        if (uri)
+                *uri = g_strdup (uri_result);
+
+        g_free (uri_result);
 
         return book;
 }
--- addressbook/tests/ebook/ebook-test-utils.h
+++ addressbook/tests/ebook/ebook-test-utils.h
@@ -22,18 +22,46 @@
 #ifndef _EBOOK_TEST_UTILS_H
 #define _EBOOK_TEST_UTILS_H
 
+#include <glib.h>
 #include <libebook/e-book.h>
+
+/* TODO: build up a library of vcards as separate files; don't hardcode here */
+#define EBOOK_TEST_UTILS_VCARD_SIMPLE \
+"BEGIN:VCARD\n\
+FN:Foo Bar\n\
+EMAIL;INTERNET:foo.bar@example.org\n\
+END:VCARD"
 
 typedef struct {
         GSourceFunc    cb;
         gpointer       user_data;
 } EBookTestClosure;
 
-EBook* ebook_test_utils_book_new_temp (char **uri);
-void   ebook_test_utils_book_open (EBook *book, gboolean only_if_exists);
-void   ebook_test_utils_book_remove (EBook *book);
-void   ebook_test_utils_book_async_remove (EBook          *book,
-                                           GSourceFunc     callback,
-                                           gpointer        user_data);
+EBook*
+ebook_test_utils_book_new_temp (char **uri);
+
+char*
+ebook_test_utils_book_add_contact (EBook    *book,
+                                   EContact *contact);
+
+EContact*
+ebook_test_utils_book_get_contact (EBook      *book,
+                                   const char *uid);
+void   
+ebook_test_utils_book_async_get_contact (EBook       *book,
+                                         const char  *uid,
+                                         GSourceFunc  callback,
+                                         gpointer     user_data);
+
+void
+ebook_test_utils_book_open (EBook    *book,
+                            gboolean  only_if_exists);
+
+void
+ebook_test_utils_book_remove (EBook *book);
+void
+ebook_test_utils_book_async_remove (EBook          *book,
+                                    GSourceFunc     callback,
+                                    gpointer        user_data);
 
 #endif /* _EBOOK_TEST_UTILS_H */
