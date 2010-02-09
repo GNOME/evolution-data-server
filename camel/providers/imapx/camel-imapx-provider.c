40ce7ececb92e32bc910bbe9138083fe8f0e972c
Chenthill Palanisamy <pchenthill@novell.com>
2010-02-09 18:00:20 +0530

    Bug 609375 -  Duplicate mnemonic in imapx preference

--- camel/providers/imapx/camel-imapx-provider.c
+++ camel/providers/imapx/camel-imapx-provider.c
@@ -52,8 +52,8 @@ CamelProviderConfEntry imapx_conf_entries[] = {
 	  N_("_Use custom command to connect to server"), "0" },
 	{ CAMEL_PROVIDER_CONF_ENTRY, "command", "use_command",
 	  N_("Command:"), "ssh -C -l %u %h exec /usr/sbin/imapd" },
-	{ CAMEL_PROVIDER_CONF_CHECKSPIN, "cached_conn", NULL,
-	  N_("Number of _cached connections to use"), "y:1:5:7" },
+	{ CAMEL_PROVIDER_CONF_CHECKSPIN, "cachedconn", NULL,
+	  N_("Numbe_r of cached connections to use"), "y:1:5:7" },
 	{ CAMEL_PROVIDER_CONF_SECTION_END },
 #endif
 	{ CAMEL_PROVIDER_CONF_SECTION_START, "folders", NULL,
