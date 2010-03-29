8d2e58edc64e7bb92a59a13030ca64f2283bd69b
Chenthill Palanisamy <pchenthill@novell.com>
2010-03-29 18:11:09 +0530

    Bug 614056 - evolution-data-server-2.29.92 failed to build

--- camel/providers/imapx/camel-imapx-tokenise.h
+++ camel/providers/imapx/camel-imapx-tokenise.h
@@ -1,5 +1,5 @@
-/* ANSI-C code produced by gperf version 3.0.3 */
-/* Command-line: /usr/bin/gperf -H imapx_hash -N imapx_tokenise_struct -L ANSI-C -o -t -k'1,$' camel-imapx-tokens.txt  */
+/* ANSI-C code produced by gperf version 3.0.2 */
+/* Command-line: /usr/bin/gperf -H imapx_hash -N imapx_tokenise_struct -L ANSI-C -o -t -k'1,$' --output-file=camel-imapx-tokenise.h camel-imapx-tokens.txt  */
 
 #if !((' ' == 32) && ('!' == 33) && ('"' == 34) && ('#' == 35) \
       && ('%' == 37) && ('&' == 38) && ('\'' == 39) && ('(' == 40) \
@@ -30,6 +30,7 @@
 
 #line 3 "camel-imapx-tokens.txt"
 struct _imapx_keyword {const gchar *name; camel_imapx_id_t id; };
+struct _imapx_keyword *imapx_tokenise_struct (register const char *str, register unsigned int len);
 
 #define TOTAL_KEYWORDS 36
 #define MIN_WORD_LENGTH 2
@@ -82,9 +83,6 @@ imapx_hash (register const char *str, register unsigned int len)
 
 #ifdef __GNUC__
 __inline
-#ifdef __GNUC_STDC_INLINE__
-__attribute__ ((__gnu_inline__))
-#endif
 #endif
 struct _imapx_keyword *
 imapx_tokenise_struct (register const char *str, register unsigned int len)
@@ -92,86 +90,86 @@ imapx_tokenise_struct (register const char *str, register unsigned int len)
   static struct _imapx_keyword wordlist[] =
     {
       {""}, {""}, {""}, {""}, {""},
-#line 26 "camel-imapx-tokens.txt"
+#line 27 "camel-imapx-tokens.txt"
       {"PARSE",		IMAPX_PARSE},
       {""},
-#line 15 "camel-imapx-tokens.txt"
+#line 16 "camel-imapx-tokens.txt"
       {"EXPUNGE",	IMAPX_EXPUNGE},
-#line 13 "camel-imapx-tokens.txt"
+#line 14 "camel-imapx-tokens.txt"
       {"ENVELOPE",	IMAPX_ENVELOPE},
       {""},
-#line 30 "camel-imapx-tokens.txt"
-      {"READ-WRITE",	IMAPX_READ_WRITE},
-#line 33 "camel-imapx-tokens.txt"
-      {"RFC822.SIZE",	IMAPX_RFC822_SIZE},
-#line 28 "camel-imapx-tokens.txt"
-      {"PREAUTH",	IMAPX_PREAUTH},
-#line 32 "camel-imapx-tokens.txt"
-      {"RFC822.HEADER",	IMAPX_RFC822_HEADER},
-#line 29 "camel-imapx-tokens.txt"
-      {"READ-ONLY",	IMAPX_READ_ONLY},
-#line 16 "camel-imapx-tokens.txt"
-      {"FETCH",		IMAPX_FETCH},
 #line 31 "camel-imapx-tokens.txt"
+      {"READ-WRITE",	IMAPX_READ_WRITE},
+#line 34 "camel-imapx-tokens.txt"
+      {"RFC822.SIZE",	IMAPX_RFC822_SIZE},
+#line 29 "camel-imapx-tokens.txt"
+      {"PREAUTH",	IMAPX_PREAUTH},
+#line 33 "camel-imapx-tokens.txt"
+      {"RFC822.HEADER",	IMAPX_RFC822_HEADER},
+#line 30 "camel-imapx-tokens.txt"
+      {"READ-ONLY",	IMAPX_READ_ONLY},
+#line 17 "camel-imapx-tokens.txt"
+      {"FETCH",		IMAPX_FETCH},
+#line 32 "camel-imapx-tokens.txt"
       {"RECENT",		IMAPX_RECENT},
-#line 25 "camel-imapx-tokens.txt"
+#line 26 "camel-imapx-tokens.txt"
       {"OK",		IMAPX_OK},
-#line 10 "camel-imapx-tokens.txt"
+#line 11 "camel-imapx-tokens.txt"
       {"BYE",		IMAPX_BYE},
-#line 36 "camel-imapx-tokens.txt"
+#line 37 "camel-imapx-tokens.txt"
       {"TRYCREATE",	IMAPX_TRYCREATE},
       {""},
-#line 34 "camel-imapx-tokens.txt"
+#line 35 "camel-imapx-tokens.txt"
       {"RFC822.TEXT",	IMAPX_RFC822_TEXT},
-#line 18 "camel-imapx-tokens.txt"
+#line 19 "camel-imapx-tokens.txt"
       {"INTERNALDATE",	IMAPX_INTERNALDATE},
       {""},
-#line 8 "camel-imapx-tokens.txt"
-      {"BODY",		IMAPX_BODY},
-#line 11 "camel-imapx-tokens.txt"
-      {"CAPABILITY",	IMAPX_CAPABILITY},
-#line 38 "camel-imapx-tokens.txt"
-      {"UIDVALIDITY",	IMAPX_UIDVALIDITY},
-#line 40 "camel-imapx-tokens.txt"
-      {"UIDNEXT",	IMAPX_UIDNEXT},
 #line 9 "camel-imapx-tokens.txt"
+      {"BODY",		IMAPX_BODY},
+#line 12 "camel-imapx-tokens.txt"
+      {"CAPABILITY",	IMAPX_CAPABILITY},
+#line 39 "camel-imapx-tokens.txt"
+      {"UIDVALIDITY",	IMAPX_UIDVALIDITY},
+#line 41 "camel-imapx-tokens.txt"
+      {"UIDNEXT",	IMAPX_UIDNEXT},
+#line 10 "camel-imapx-tokens.txt"
       {"BODYSTRUCTURE",	IMAPX_BODYSTRUCTURE},
       {""}, {""},
-#line 14 "camel-imapx-tokens.txt"
+#line 15 "camel-imapx-tokens.txt"
       {"EXISTS",		IMAPX_EXISTS},
-#line 23 "camel-imapx-tokens.txt"
+#line 24 "camel-imapx-tokens.txt"
       {"NEWNAME",	IMAPX_NEWNAME},
-#line 37 "camel-imapx-tokens.txt"
+#line 38 "camel-imapx-tokens.txt"
       {"UID",		IMAPX_UID},
-#line 22 "camel-imapx-tokens.txt"
+#line 23 "camel-imapx-tokens.txt"
       {"NAMESPACE",	IMAPX_NAMESPACE},
-#line 17 "camel-imapx-tokens.txt"
+#line 18 "camel-imapx-tokens.txt"
       {"FLAGS",		IMAPX_FLAGS},
       {""},
-#line 12 "camel-imapx-tokens.txt"
+#line 13 "camel-imapx-tokens.txt"
       {"COPYUID",	IMAPX_COPYUID},
-#line 7 "camel-imapx-tokens.txt"
+#line 8 "camel-imapx-tokens.txt"
       {"BAD",		IMAPX_BAD},
-#line 27 "camel-imapx-tokens.txt"
+#line 28 "camel-imapx-tokens.txt"
       {"PERMANENTFLAGS",	IMAPX_PERMANENTFLAGS},
-#line 5 "camel-imapx-tokens.txt"
+#line 6 "camel-imapx-tokens.txt"
       {"ALERT",          IMAPX_ALERT},
-#line 39 "camel-imapx-tokens.txt"
+#line 40 "camel-imapx-tokens.txt"
       {"UNSEEN",		IMAPX_UNSEEN},
-#line 24 "camel-imapx-tokens.txt"
+#line 25 "camel-imapx-tokens.txt"
       {"NO",		IMAPX_NO},
-#line 21 "camel-imapx-tokens.txt"
+#line 22 "camel-imapx-tokens.txt"
       {"MESSAGES",	IMAPX_MESSAGES},
-#line 19 "camel-imapx-tokens.txt"
+#line 20 "camel-imapx-tokens.txt"
       {"LIST",		IMAPX_LIST},
       {""}, {""}, {""}, {""},
-#line 20 "camel-imapx-tokens.txt"
+#line 21 "camel-imapx-tokens.txt"
       {"LSUB",		IMAPX_LSUB},
       {""}, {""}, {""}, {""},
-#line 6 "camel-imapx-tokens.txt"
+#line 7 "camel-imapx-tokens.txt"
       {"APPENDUID",	IMAPX_APPENDUID},
       {""},
-#line 35 "camel-imapx-tokens.txt"
+#line 36 "camel-imapx-tokens.txt"
       {"STATUS",		IMAPX_STATUS}
     };
 
