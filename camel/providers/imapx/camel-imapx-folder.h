82696d86c022cff5cb4ed9f109497e5a2bccf24e
David Woodhouse <David.Woodhouse@intel.com>
2010-06-29 16:45:56 +0100

    Fix APPENDUID handling of uidvalidity.
    We shouldn't be comparing with is->uidvalidity; that's just short-term
    storage for use _during_ a SELECT. Compare with ifolder->uidvalidity instead.
    
    Oh, and let's try to make sure that's actually set somewhere...
    (cherry picked from commit 573ad77df8814a5f72593292bc6e96ef4140e1a3)

--- camel/providers/imapx/camel-imapx-folder.h
+++ camel/providers/imapx/camel-imapx-folder.h
@@ -48,6 +48,7 @@ typedef struct _CamelIMAPXFolder {
 	guint32 exists_on_server;
 	guint32 unread_on_server;
 	guint64 modseq_on_server;
+	guint64 uidvalidity_on_server;
 	guint32 uidnext_on_server;
 
 	/* hash table of UIDs to ignore as recent when updating folder */
