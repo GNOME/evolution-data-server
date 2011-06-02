9ca324e9c5b89b982e40593d3afc97eb011ee832
Chenthill Palanisamy <pchenthill@novell.com>
2011-06-02 15:38:46 +0530

    CamelDB: use a RW lock to prevent reads while write is in progress

--- camel/camel-store.c
+++ camel/camel-store.c
@@ -229,10 +229,6 @@ store_finalize (GObject *object)
 	if (store->cdb_r != NULL) {
 		camel_db_close (store->cdb_r);
 		store->cdb_r = NULL;
-	}
-
-	if (store->cdb_w != NULL) {
-		camel_db_close (store->cdb_w);
 		store->cdb_w = NULL;
 	}
 
@@ -319,8 +315,8 @@ store_construct (CamelService *service,
 		return FALSE;
 	}
 
-	/* This is for writing to the store */
-	store->cdb_w = camel_db_clone (store->cdb_r, error);
+	/* keep cb_w to not break the ABI */
+	store->cdb_w = store->cdb_r;
 
 	if (camel_url_get_param(url, "filter"))
 		store->flags |= CAMEL_STORE_FILTER_INBOX;
