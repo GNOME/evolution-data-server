ffd8689da2f2c3c87f12c196695558c49e885382
Milan Crha <mcrha@redhat.com>
2012-03-07 19:30:54 +0100

    Bug #664186 - Maildir scan checks incorrectly for folder name prefix

--- camel/providers/local/camel-maildir-store.c
+++ camel/providers/local/camel-maildir-store.c
@@ -607,10 +607,14 @@ scan_dirs (CamelStore *store,
 			short_name++;
 
 		if ((g_ascii_strcasecmp ((*topfi)->full_name, "Inbox") != 0 
-		    && !g_str_has_prefix (full_name, (*topfi)->full_name))
+		    && (!g_str_has_prefix (full_name, (*topfi)->full_name) ||
+			(full_name[strlen((*topfi)->full_name)] != '\0' &&
+			 full_name[strlen((*topfi)->full_name)] != '/')))
 		    || (!can_inbox_sibling
 		    && g_ascii_strcasecmp ((*topfi)->full_name, "Inbox") == 0 
-		    && !g_str_has_prefix (full_name, (*topfi)->full_name))) {
+		    && !g_str_has_prefix (full_name, (*topfi)->full_name)) ||
+			(full_name[strlen((*topfi)->full_name)] != '\0' &&
+			 full_name[strlen((*topfi)->full_name)] != '/')) {
 			g_free (full_name);
 			continue;
 		}
