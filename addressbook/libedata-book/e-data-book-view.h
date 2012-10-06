0092b40ac170e4f80d8202d4972cad4813861191
Matthew Barnes <mbarnes@redhat.com>
2012-10-06 12:58:54 -0400

    e-data-book-view.h readability cleanups.

--- addressbook/libedata-book/e-data-book-view.h
+++ addressbook/libedata-book/e-data-book-view.h
@@ -25,21 +25,33 @@
 #error "Only <libedata-book/libedata-book.h> should be included directly."
 #endif
 
-#ifndef __E_DATA_BOOK_VIEW_H__
-#define __E_DATA_BOOK_VIEW_H__
+#ifndef E_DATA_BOOK_VIEW_H
+#define E_DATA_BOOK_VIEW_H
 
 #include <libebook/libebook.h>
 
 #include <libedata-book/e-book-backend-sexp.h>
 
-G_BEGIN_DECLS
+/* Standard GObject macros */
+#define E_TYPE_DATA_BOOK_VIEW \
+	(e_data_book_view_get_type ())
+#define E_DATA_BOOK_VIEW(obj) \
+	(G_TYPE_CHECK_INSTANCE_CAST \
+	((obj), E_TYPE_DATA_BOOK_VIEW, EDataBookView))
+#define E_DATA_BOOK_VIEW_CLASS(cls) \
+	(G_TYPE_CHECK_CLASS_CAST \
+	((cls), E_TYPE_DATA_BOOK_VIEW, EDataBookViewClass))
+#define E_IS_DATA_BOOK_VIEW(obj) \
+	(G_TYPE_CHECK_INSTANCE_TYPE \
+	((obj), E_TYPE_DATA_BOOK_VIEW))
+#define E_IS_DATA_BOOK_VIEW_CLASS(cls) \
+	(G_TYPE_CHECK_CLASS_TYPE \
+	((cls), E_TYPE_DATA_BOOK_VIEW))
+#define E_DATA_BOOK_VIEW_GET_CLASS(obj) \
+	(G_TYPE_INSTANCE_GET_CLASS \
+	((obj), E_TYPE_DATA_BOOK_VIEW, EDataBookViewClass))
 
-#define E_TYPE_DATA_BOOK_VIEW        (e_data_book_view_get_type ())
-#define E_DATA_BOOK_VIEW(o)          (G_TYPE_CHECK_INSTANCE_CAST ((o), E_TYPE_DATA_BOOK_VIEW, EDataBookView))
-#define E_DATA_BOOK_VIEW_CLASS(k)    (G_TYPE_CHECK_CLASS_CAST((k), E_TYPE_DATA_BOOK_VIEW, EDataBookViewClass))
-#define E_IS_DATA_BOOK_VIEW(o)       (G_TYPE_CHECK_INSTANCE_TYPE ((o), E_TYPE_DATA_BOOK_VIEW))
-#define E_IS_DATA_BOOK_VIEW_CLASS(k) (G_TYPE_CHECK_CLASS_TYPE ((k), E_TYPE_DATA_BOOK_VIEW))
-#define E_DATA_BOOK_VIEW_GET_CLASS(k) (G_TYPE_INSTANCE_GET_CLASS ((obj), E_TYPE_DATA_BOOK_VIEW, EDataBookView))
+G_BEGIN_DECLS
 
 struct _EDataBook;
 struct _EBookBackend;
@@ -57,27 +69,50 @@ struct _EDataBookViewClass {
 	GObjectClass parent;
 };
 
-GType			e_data_book_view_get_type		(void);
-EDataBookView *		e_data_book_view_new			(struct _EDataBook *book, const gchar *card_query, EBookBackendSExp *card_sexp);
-guint			e_data_book_view_register_gdbus_object	(EDataBookView *query, GDBusConnection *connection, const gchar *object_path, GError **error);
+GType		e_data_book_view_get_type	(void) G_GNUC_CONST;
+EDataBookView *	e_data_book_view_new		(struct _EDataBook *book,
+						 const gchar *card_query,
+						 EBookBackendSExp *card_sexp);
+guint		e_data_book_view_register_gdbus_object
+						(EDataBookView *query,
+						 GDBusConnection *connection,
+						 const gchar *object_path,
+						 GError **error);
 
-const gchar *		e_data_book_view_get_card_query		(EDataBookView *book_view);
-EBookBackendSExp *	e_data_book_view_get_card_sexp		(EDataBookView *book_view);
-struct _EBookBackend *	e_data_book_view_get_backend		(EDataBookView *book_view);
-EBookClientViewFlags    e_data_book_view_get_flags              (EDataBookView *book_view);
-void			e_data_book_view_notify_update		(EDataBookView *book_view, const EContact *contact);
+const gchar *	e_data_book_view_get_card_query	(EDataBookView *book_view);
+EBookBackendSExp *
+		e_data_book_view_get_card_sexp	(EDataBookView *book_view);
+struct _EBookBackend *
+		e_data_book_view_get_backend	(EDataBookView *book_view);
+EBookClientViewFlags
+		e_data_book_view_get_flags	(EDataBookView *book_view);
+void		e_data_book_view_notify_update	(EDataBookView *book_view,
+						 const EContact *contact);
 
-void			e_data_book_view_notify_update_vcard	(EDataBookView *book_view, const gchar *id, const gchar *vcard);
-void			e_data_book_view_notify_update_prefiltered_vcard (EDataBookView *book_view, const gchar *id, const gchar *vcard);
+void		e_data_book_view_notify_update_vcard
+						(EDataBookView *book_view,
+						 const gchar *id,
+						 const gchar *vcard);
+void		e_data_book_view_notify_update_prefiltered_vcard
+						(EDataBookView *book_view,
+						 const gchar *id,
+						 const gchar *vcard);
 
-void			e_data_book_view_notify_remove		(EDataBookView *book_view, const gchar *id);
-void			e_data_book_view_notify_complete	(EDataBookView *book_view, const GError *error);
-void			e_data_book_view_notify_progress        (EDataBookView *book_view, guint percent, const gchar *message);
-void			e_data_book_view_ref			(EDataBookView *book_view);
-void			e_data_book_view_unref			(EDataBookView *book_view);
+void		e_data_book_view_notify_remove	(EDataBookView *book_view,
+						 const gchar *id);
+void		e_data_book_view_notify_complete
+						(EDataBookView *book_view,
+						 const GError *error);
+void		e_data_book_view_notify_progress
+						(EDataBookView *book_view,
+						 guint percent,
+						 const gchar *message);
+void		e_data_book_view_ref		(EDataBookView *book_view);
+void		e_data_book_view_unref		(EDataBookView *book_view);
 
-/* const */ GHashTable *e_data_book_view_get_fields_of_interest	(EDataBookView *view);
+GHashTable *	e_data_book_view_get_fields_of_interest
+						(EDataBookView *view);
 
 G_END_DECLS
 
-#endif /* __E_DATA_BOOK_VIEW_H__ */
+#endif /* E_DATA_BOOK_VIEW_H */
