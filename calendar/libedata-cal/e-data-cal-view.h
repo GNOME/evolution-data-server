206cc61fc3d83630a9e580e76e1628777de39ea6
Matthew Barnes <mbarnes@redhat.com>
2012-10-06 16:37:00 -0400

    e-data-cal-view.h readability cleanups.

--- calendar/libedata-cal/e-data-cal-view.h
+++ calendar/libedata-cal/e-data-cal-view.h
@@ -27,14 +27,26 @@
 
 #include <libecal/libecal.h>
 
+/* Standard GObject macros */
+#define E_DATA_CAL_VIEW_TYPE \
+	(e_data_cal_view_get_type ())
+#define E_DATA_CAL_VIEW(obj) \
+	(G_TYPE_CHECK_INSTANCE_CAST \
+	((obj), E_DATA_CAL_VIEW_TYPE, EDataCalView))
+#define E_DATA_CAL_VIEW_CLASS(klass) \
+	(G_TYPE_CHECK_CLASS_CAST \
+	((klass), E_DATA_CAL_VIEW_TYPE, EDataCalViewClass))
+#define E_IS_DATA_CAL_VIEW(obj) \
+	(G_TYPE_CHECK_INSTANCE_TYPE \
+	((obj), E_DATA_CAL_VIEW_TYPE))
+#define E_IS_DATA_CAL_VIEW_CLASS(cls) \
+	(G_TYPE_CHECK_CLASS_TYPE \
+	((cls), E_DATA_CAL_VIEW_TYPE))
+#define E_DATA_CAL_VIEW_GET_CLASS(obj) \
+	(G_TYPE_INSTANCE_GET_CLASS \
+	((obj), E_DATA_CAL_VIEW_TYPE, EDataCalViewClass))
+
 G_BEGIN_DECLS
-
-
-
-#define E_DATA_CAL_VIEW_TYPE            (e_data_cal_view_get_type ())
-#define E_DATA_CAL_VIEW(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), E_DATA_CAL_VIEW_TYPE, EDataCalView))
-#define E_DATA_CAL_VIEW_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), E_DATA_CAL_VIEW_TYPE, EDataCalViewClass))
-#define E_IS_DATA_CAL_VIEW(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), E_DATA_CAL_VIEW_TYPE))
 
 struct _ECalBackend;
 struct _ECalBackendSExp;
@@ -52,39 +64,74 @@ struct _EDataCalViewClass {
 	GObjectClass parent_class;
 };
 
-GType			e_data_cal_view_get_type			(void);
-EDataCalView *		e_data_cal_view_new				(struct _ECalBackend *backend, struct _ECalBackendSExp *sexp);
-guint			e_data_cal_view_register_gdbus_object		(EDataCalView *view, GDBusConnection *connection, const gchar *object_path, GError **error);
-const gchar *		e_data_cal_view_get_text			(EDataCalView *view);
+GType		e_data_cal_view_get_type	(void) G_GNUC_CONST;
+EDataCalView *	e_data_cal_view_new		(struct _ECalBackend *backend,
+						 struct _ECalBackendSExp *sexp);
+guint		e_data_cal_view_register_gdbus_object
+						(EDataCalView *view,
+						 GDBusConnection *connection,
+						 const gchar *object_path,
+						 GError **error);
+const gchar *	e_data_cal_view_get_text	(EDataCalView *view);
 struct _ECalBackendSExp *
-			e_data_cal_view_get_object_sexp			(EDataCalView *view);
-gboolean		e_data_cal_view_object_matches			(EDataCalView *view, const gchar *object);
-gboolean		e_data_cal_view_component_matches		(EDataCalView *view, ECalComponent *component);
-gboolean		e_data_cal_view_is_started			(EDataCalView *view);
-gboolean		e_data_cal_view_is_completed			(EDataCalView *view);
-gboolean		e_data_cal_view_is_stopped			(EDataCalView *view);
-GHashTable *		e_data_cal_view_get_fields_of_interest		(EDataCalView *view);
-ECalClientViewFlags    e_data_cal_view_get_flags			(EDataCalView *view);
+		e_data_cal_view_get_object_sexp	(EDataCalView *view);
+gboolean	e_data_cal_view_object_matches	(EDataCalView *view,
+						 const gchar *object);
+gboolean	e_data_cal_view_component_matches
+						(EDataCalView *view,
+						 ECalComponent *component);
+gboolean	e_data_cal_view_is_started	(EDataCalView *view);
+gboolean	e_data_cal_view_is_completed	(EDataCalView *view);
+gboolean	e_data_cal_view_is_stopped	(EDataCalView *view);
+GHashTable *	e_data_cal_view_get_fields_of_interest
+						(EDataCalView *view);
+ECalClientViewFlags
+		e_data_cal_view_get_flags	(EDataCalView *view);
 
-gchar *			e_data_cal_view_get_component_string		(EDataCalView *view, /* const */ ECalComponent *component);
+gchar *		e_data_cal_view_get_component_string
+						(EDataCalView *view,
+						 ECalComponent *component);
 
-void			e_data_cal_view_notify_components_added		(EDataCalView *view, const GSList *ecalcomponents);
-void			e_data_cal_view_notify_components_added_1	(EDataCalView *view, /* const */ ECalComponent *component);
-void			e_data_cal_view_notify_components_modified	(EDataCalView *view, const GSList *ecalcomponents);
-void			e_data_cal_view_notify_components_modified_1	(EDataCalView *view, /* const */ ECalComponent *component);
+void		e_data_cal_view_notify_components_added
+						(EDataCalView *view,
+						 const GSList *ecalcomponents);
+void		e_data_cal_view_notify_components_added_1
+						(EDataCalView *view,
+						 ECalComponent *component);
+void		e_data_cal_view_notify_components_modified
+						(EDataCalView *view,
+						 const GSList *ecalcomponents);
+void		e_data_cal_view_notify_components_modified_1
+						(EDataCalView *view,
+						 ECalComponent *component);
 
 #ifndef E_CAL_DISABLE_DEPRECATED
-void			e_data_cal_view_notify_objects_added		(EDataCalView *view, const GSList *objects);
-void			e_data_cal_view_notify_objects_added_1		(EDataCalView *view, const gchar *object);
-void			e_data_cal_view_notify_objects_modified		(EDataCalView *view, const GSList *objects);
-void			e_data_cal_view_notify_objects_modified_1	(EDataCalView *view, const gchar *object);
+void		e_data_cal_view_notify_objects_added
+						(EDataCalView *view,
+						 const GSList *objects);
+void		e_data_cal_view_notify_objects_added_1
+						(EDataCalView *view,
+						 const gchar *object);
+void		e_data_cal_view_notify_objects_modified
+						(EDataCalView *view,
+						 const GSList *objects);
+void		e_data_cal_view_notify_objects_modified_1
+						(EDataCalView *view,
+						 const gchar *object);
 #endif
 
-void			e_data_cal_view_notify_objects_removed		(EDataCalView *view, const GSList *ids);
-void			e_data_cal_view_notify_objects_removed_1	(EDataCalView *view, const ECalComponentId *id);
-void			e_data_cal_view_notify_progress			(EDataCalView *view, gint percent, const gchar *message);
-void			e_data_cal_view_notify_complete			(EDataCalView *view, const GError *error);
+void		e_data_cal_view_notify_objects_removed
+						(EDataCalView *view,
+						 const GSList *ids);
+void		e_data_cal_view_notify_objects_removed_1
+						(EDataCalView *view,
+						 const ECalComponentId *id);
+void		e_data_cal_view_notify_progress	(EDataCalView *view,
+						 gint percent,
+						 const gchar *message);
+void		e_data_cal_view_notify_complete	(EDataCalView *view,
+						 const GError *error);
 
 G_END_DECLS
 
-#endif
+#endif /* E_DATA_CAL_VIEW_H */
