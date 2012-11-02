8c4934cd6984344144599ffba1708cfd10718756
Matthew Barnes <mbarnes@redhat.com>
2012-11-02 09:13:53 -0400

    e-book-view.h cleanups.

--- addressbook/libebook/e-book-view.h
+++ addressbook/libebook/e-book-view.h
@@ -12,67 +12,79 @@
 #error "Only <libebook/libebook.h> should be included directly."
 #endif
 
-#ifndef __E_BOOK_VIEW_H__
-#define __E_BOOK_VIEW_H__
-
 #ifndef E_BOOK_DISABLE_DEPRECATED
+
+#ifndef E_BOOK_VIEW_H
+#define E_BOOK_VIEW_H
 
 #include <glib-object.h>
 #include "e-book-types.h"
 
-#define E_TYPE_BOOK_VIEW           (e_book_view_get_type ())
-#define E_BOOK_VIEW(o)             (G_TYPE_CHECK_INSTANCE_CAST ((o), E_TYPE_BOOK_VIEW, EBookView))
-#define E_BOOK_VIEW_CLASS(k)       (G_TYPE_CHECK_CLASS_CAST((k), E_TYPE_BOOK_VIEW, EBookViewClass))
-#define E_IS_BOOK_VIEW(o)          (G_TYPE_CHECK_INSTANCE_TYPE ((o), E_TYPE_BOOK_VIEW))
-#define E_IS_BOOK_VIEW_CLASS(k)    (G_TYPE_CHECK_CLASS_TYPE ((k), E_TYPE_BOOK_VIEW))
-#define E_BOOK_VIEW_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS ((obj), E_TYPE_BOOK_VIEW, EBookViewClass))
+/* Standard GObject macros */
+#define E_TYPE_BOOK_VIEW \
+	(e_book_view_get_type ())
+#define E_BOOK_VIEW(obj) \
+	(G_TYPE_CHECK_INSTANCE_CAST \
+	((obj), E_TYPE_BOOK_VIEW, EBookView))
+#define E_BOOK_VIEW_CLASS(cls) \
+	(G_TYPE_CHECK_CLASS_CAST \
+	((cls), E_TYPE_BOOK_VIEW, EBookViewClass))
+#define E_IS_BOOK_VIEW(obj) \
+	(G_TYPE_CHECK_INSTANCE_TYPE \
+	((obj), E_TYPE_BOOK_VIEW))
+#define E_IS_BOOK_VIEW_CLASS(cls) \
+	(G_TYPE_CHECK_CLASS_TYPE \
+	((cls), E_TYPE_BOOK_VIEW))
+#define E_BOOK_VIEW_GET_CLASS(obj) \
+	(G_TYPE_INSTANCE_GET_CLASS \
+	((obj), E_TYPE_BOOK_VIEW, EBookViewClass))
 
 G_BEGIN_DECLS
 
-typedef struct _EBookView        EBookView;
-typedef struct _EBookViewClass   EBookViewClass;
+typedef struct _EBookView EBookView;
+typedef struct _EBookViewClass EBookViewClass;
 typedef struct _EBookViewPrivate EBookViewPrivate;
 
-struct _EBook;  /* Forward reference */
+struct _EBook;
 
 struct _EBookView {
-	GObject     parent;
-	/*< private >*/
+	GObject parent;
 	EBookViewPrivate *priv;
 };
 
 struct _EBookViewClass {
-	GObjectClass parent;
+	GObjectClass parent_class;
 
-	/*
-	 * Signals.
-	 */
-	void (* contacts_changed)  (EBookView *book_view, const GList *contacts);
-	void (* contacts_removed)  (EBookView *book_view, const GList *ids);
-	void (* contacts_added)    (EBookView *book_view, const GList *contacts);
-	#ifndef E_BOOK_DISABLE_DEPRECATED
-	void (* sequence_complete) (EBookView *book_view, EBookViewStatus status);
-	#endif
-	void (* view_complete) (EBookView *book_view, EBookViewStatus status, const gchar *error_msg);
-	void (* status_message)    (EBookView *book_view, const gchar *message);
+	/* Signals */
+	void		(*contacts_changed)	(EBookView *book_view,
+						 const GList *contacts);
+	void		(*contacts_removed)	(EBookView *book_view,
+						 const GList *ids);
+	void		(*contacts_added)	(EBookView *book_view,
+						 const GList *contacts);
+	void		(*sequence_complete)	(EBookView *book_view,
+						 EBookViewStatus status);
+	void		(*view_complete)	(EBookView *book_view,
+						 EBookViewStatus status,
+						 const gchar *error_msg);
+	void		(*status_message)	(EBookView *book_view,
+						 const gchar *message);
 
 	/* Padding for future expansion */
-	void (*_ebook_reserved0) (void);
-	void (*_ebook_reserved1) (void);
-	void (*_ebook_reserved2) (void);
-	void (*_ebook_reserved3) (void);
-	void (*_ebook_reserved4) (void);
+	void		(*_ebook_reserved0)	(void);
+	void		(*_ebook_reserved1)	(void);
+	void		(*_ebook_reserved2)	(void);
+	void		(*_ebook_reserved3)	(void);
+	void		(*_ebook_reserved4)	(void);
 };
 
-GType			e_book_view_get_type		(void);
-
-void			e_book_view_start		(EBookView *book_view);
-void			e_book_view_stop		(EBookView *book_view);
-
-struct _EBook *		e_book_view_get_book		(EBookView *book_view);
+GType		e_book_view_get_type		(void);
+struct _EBook *	e_book_view_get_book		(EBookView *book_view);
+void		e_book_view_start		(EBookView *book_view);
+void		e_book_view_stop		(EBookView *book_view);
 
 G_END_DECLS
 
-#endif /* E_BOOK_DISABLE_DEPRECATED */
+#endif /* E_BOOK_VIEW_H */
 
-#endif /* __E_BOOK_VIEW_H__ */
+#endif /* E_BOOK_DISABLE_DEPRECATED */
