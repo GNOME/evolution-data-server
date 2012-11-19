c588b82d8cc8fa991203890b34803167e505b467
Tristan Van Berkom <tristanvb@openismus.com>
2012-11-19 20:19:43 +0900

    Added e_util_utf8_normalize()
    A utility function to lowercase/remove accents from a utf8 string

--- libedataserver/e-data-server-util.h
+++ libedataserver/e-data-server-util.h
@@ -52,6 +52,7 @@ gchar *		e_util_utf8_remove_accents	(const gchar *str);
 gchar *		e_util_utf8_make_valid		(const gchar *str);
 gchar *		e_util_utf8_data_make_valid	(const gchar *data,
 						 gsize data_bytes);
+gchar *         e_util_utf8_normalize           (const gchar *str);
 const gchar *   e_util_ensure_gdbus_string	(const gchar *str,
 						 gchar **gdbus_str);
 guint64		e_util_gthread_id		(GThread *thread);
