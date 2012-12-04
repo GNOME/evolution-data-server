20a33e4f2241cbc20018f2967f8f07dccf5bf033
Tristan Van Berkom <tristanvb@openismus.com>
2012-12-04 18:16:23 +0900

    test-client-custom-summary: Added a query on the EVC_N vcard field

--- tests/libebook/client/test-client-custom-summary.c
+++ tests/libebook/client/test-client-custom-summary.c
@@ -237,6 +237,21 @@ main (gint argc,
 	e_book_query_unref (query);
 	g_free (sexp);
 
+	/* Query name component */
+	query = e_book_query_vcard_field_test(EVC_N, E_BOOK_QUERY_IS, "Janet");
+	sexp = e_book_query_to_string (query);
+
+	if (!e_book_client_get_contacts_sync (book_client, sexp, &results, NULL, &error)) {
+		report_error ("get contacts", &error);
+		g_object_unref (book_client);
+		return 1;
+	}
+	g_assert_cmpint (g_slist_length (results), ==, 1);
+	e_util_free_object_slist (results);
+	e_book_query_unref (query);
+	g_free (sexp);
+
+
 	/* Query email suffix... fetching uids */
 	query = e_book_query_field_test (E_CONTACT_EMAIL, E_BOOK_QUERY_ENDS_WITH, "jackson.com");
 	sexp = e_book_query_to_string (query);
