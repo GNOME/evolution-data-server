9ef3e7421a54f6fef094366f43b939c4e28239a8
Mathias Hasselmann <mathias@openismus.com>
2012-12-11 10:53:00 +0100

    libedata-book: Use slice allocator for EbSdbSearchData

--- addressbook/libedata-book/e-book-backend-sqlitedb.c
+++ addressbook/libedata-book/e-book-backend-sqlitedb.c
@@ -1893,7 +1893,7 @@ store_data_to_vcard (gpointer ref,
                      gchar **name)
 {
 	GSList **vcard_data = ref;
-	EbSdbSearchData *search_data = g_new0 (EbSdbSearchData, 1);
+	EbSdbSearchData *search_data = g_slice_new0 (EbSdbSearchData);
 	EContact *contact = e_contact_new ();
 	gchar *vcard;
 	gint i;
@@ -2669,7 +2669,7 @@ addto_vcard_list_cb (gpointer ref,
                      gchar **name)
 {
 	GSList **vcard_data = ref;
-	EbSdbSearchData *s_data = g_new0 (EbSdbSearchData, 1);
+	EbSdbSearchData *s_data = g_slice_new0 (EbSdbSearchData);
 
 	if (cols[0])
 		s_data->uid = g_strdup (cols[0]);
@@ -3567,7 +3567,7 @@ e_book_backend_sqlitedb_search_data_free (EbSdbSearchData *s_data)
 		g_free (s_data->uid);
 		g_free (s_data->vcard);
 		g_free (s_data->bdata);
-		g_free (s_data);
+		g_slice_free (EbSdbSearchData, s_data);
 	}
 }
 
