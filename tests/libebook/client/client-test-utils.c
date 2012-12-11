1485bfc9e6cc8ede09542b5d5d34e9e48af5e01e
Mathias Hasselmann <mathias@openismus.com>
2012-12-11 10:50:49 +0100

    tests: Add a few missing license headers

--- tests/libebook/client/client-test-utils.c
+++ tests/libebook/client/client-test-utils.c
@@ -1,4 +1,26 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * Copyright (C) 2011, 2012 Red Hat, Inc. (www.redhat.com)
+ * Copyright (C) 2012 Intel Corporation
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU Lesser General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ * Authors: Milan Crha <mcrha@redhat.com>
+ *          Matthew Barnes <mbarnes@redhat.com>
+ *          Tristan Van Berkom <tristanvb@openismus.com>
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
