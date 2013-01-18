8dcd4683d4d57d31ac978bc6e59eff8a5eed3db9
Tristan Van Berkom <tristanvb@openismus.com>
2013-01-18 15:55:32 +0900

    Added test case ensuring that new contacts have thier UID preserved.
    Asserts both that newly added contacts preserve the assigned UID and
    also that 2 contacts with the same UID cannot be added.

---
+++ tests/libebook/client/test-client-preserve-uid.c
@@ -0,0 +1,85 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+
+#include <stdlib.h>
+#include <libebook/libebook.h>
+
+#include "client-test-utils.h"
+#include "e-test-server-utils.h"
+
+#define TEST_CONTACT_UID "old-mac-donald-had-a-farm"
+
+static ETestServerClosure book_closure = { E_TEST_SERVER_ADDRESS_BOOK, NULL, 0 };
+
+static void
+test_preserve_uid (ETestServerFixture *fixture,
+		   gconstpointer       user_data)
+{
+  EBookClient *book_client;
+  EContact    *contact;
+  gchar       *vcard;
+  gchar       *uid = NULL;
+  GError      *error = NULL;
+
+  book_client = E_TEST_SERVER_UTILS_SERVICE (fixture, EBookClient);
+
+  vcard   = new_vcard_from_test_case ("simple-1");
+  contact = e_contact_new_from_vcard (vcard);
+  g_free (vcard);
+
+  e_contact_set (contact, E_CONTACT_UID, TEST_CONTACT_UID);
+
+  if (!e_book_client_add_contact_sync (book_client, contact, &uid, NULL, &error))
+    g_error ("Failed to add contact: %s", error->message);
+
+  g_assert_cmpstr (uid, ==, TEST_CONTACT_UID);
+  g_object_unref (contact);
+  g_free (uid);
+}
+
+static void
+test_uid_conflict (ETestServerFixture *fixture,
+		   gconstpointer       user_data)
+{
+  EBookClient *book_client;
+  EContact    *contact;
+  gchar       *vcard;
+  GError      *error = NULL;
+
+  book_client = E_TEST_SERVER_UTILS_SERVICE (fixture, EBookClient);
+
+  /* Hijack the first test case, ensure we already have the contact added */
+  test_preserve_uid (fixture, user_data);
+
+  vcard   = new_vcard_from_test_case ("simple-2");
+  contact = e_contact_new_from_vcard (vcard);
+  g_free (vcard);
+
+  e_contact_set (contact, E_CONTACT_UID, TEST_CONTACT_UID);
+
+  if (!e_book_client_add_contact_sync (book_client, contact, NULL, NULL, &error))
+    {
+      g_assert (g_error_matches (error, E_CLIENT_ERROR, E_CLIENT_ERROR_QUERY_REFUSED));
+      g_error_free (error);
+    }
+  else
+    g_error ("Succeeded in adding two contacts with the same UID !");
+
+  g_object_unref (contact);
+}
+
+gint
+main (gint argc,
+      gchar **argv)
+{
+#if !GLIB_CHECK_VERSION (2, 35, 1)
+	g_type_init ();
+#endif
+	g_test_init (&argc, &argv, NULL);
+
+	g_test_add ("/EBookClient/AddContact/PreserveUid", ETestServerFixture, &book_closure,
+		    e_test_server_utils_setup, test_preserve_uid, e_test_server_utils_teardown);
+	g_test_add ("/EBookClient/AddContact/UidConflict", ETestServerFixture, &book_closure,
+		    e_test_server_utils_setup, test_uid_conflict, e_test_server_utils_teardown);
+
+	return e_test_server_utils_run ();
+}
