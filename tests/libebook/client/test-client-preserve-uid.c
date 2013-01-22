1fc007b4af3579b10a91241630ddf782784eaed4
Tristan Van Berkom <tristanvb@openismus.com>
2013-01-22 15:05:26 +0900

    test-client-preserve-uid.c: Fix coding style and copyright header

--- tests/libebook/client/test-client-preserve-uid.c
+++ tests/libebook/client/test-client-preserve-uid.c
@@ -1,6 +1,6 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 /*
- * Copyright Copyright (C) 2013 Intel Corporation
+ * Copyright (C) 2013 Intel Corporation
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of version 2 of the GNU Lesser General Public
@@ -33,57 +33,55 @@ static void
 test_preserve_uid (ETestServerFixture *fixture,
 		   gconstpointer       user_data)
 {
-  EBookClient *book_client;
-  EContact    *contact;
-  gchar       *vcard;
-  gchar       *uid = NULL;
-  GError      *error = NULL;
+	EBookClient *book_client;
+	EContact    *contact;
+	gchar       *vcard;
+	gchar       *uid = NULL;
+	GError      *error = NULL;
 
-  book_client = E_TEST_SERVER_UTILS_SERVICE (fixture, EBookClient);
+	book_client = E_TEST_SERVER_UTILS_SERVICE (fixture, EBookClient);
 
-  vcard   = new_vcard_from_test_case ("simple-1");
-  contact = e_contact_new_from_vcard (vcard);
-  g_free (vcard);
+	vcard   = new_vcard_from_test_case ("simple-1");
+	contact = e_contact_new_from_vcard (vcard);
+	g_free (vcard);
 
-  e_contact_set (contact, E_CONTACT_UID, TEST_CONTACT_UID);
+	e_contact_set (contact, E_CONTACT_UID, TEST_CONTACT_UID);
 
-  if (!e_book_client_add_contact_sync (book_client, contact, &uid, NULL, &error))
-    g_error ("Failed to add contact: %s", error->message);
+	if (!e_book_client_add_contact_sync (book_client, contact, &uid, NULL, &error))
+		g_error ("Failed to add contact: %s", error->message);
 
-  g_assert_cmpstr (uid, ==, TEST_CONTACT_UID);
-  g_object_unref (contact);
-  g_free (uid);
+	g_assert_cmpstr (uid, ==, TEST_CONTACT_UID);
+	g_object_unref (contact);
+	g_free (uid);
 }
 
 static void
 test_uid_conflict (ETestServerFixture *fixture,
 		   gconstpointer       user_data)
 {
-  EBookClient *book_client;
-  EContact    *contact;
-  gchar       *vcard;
-  GError      *error = NULL;
+	EBookClient *book_client;
+	EContact    *contact;
+	gchar       *vcard;
+	GError      *error = NULL;
 
-  book_client = E_TEST_SERVER_UTILS_SERVICE (fixture, EBookClient);
+	book_client = E_TEST_SERVER_UTILS_SERVICE (fixture, EBookClient);
 
-  /* Hijack the first test case, ensure we already have the contact added */
-  test_preserve_uid (fixture, user_data);
+	/* Hijack the first test case, ensure we already have the contact added */
+	test_preserve_uid (fixture, user_data);
 
-  vcard   = new_vcard_from_test_case ("simple-2");
-  contact = e_contact_new_from_vcard (vcard);
-  g_free (vcard);
+	vcard   = new_vcard_from_test_case ("simple-2");
+	contact = e_contact_new_from_vcard (vcard);
+	g_free (vcard);
 
-  e_contact_set (contact, E_CONTACT_UID, TEST_CONTACT_UID);
+	e_contact_set (contact, E_CONTACT_UID, TEST_CONTACT_UID);
 
-  if (!e_book_client_add_contact_sync (book_client, contact, NULL, NULL, &error))
-    {
-      g_assert (g_error_matches (error, E_BOOK_CLIENT_ERROR, E_BOOK_CLIENT_ERROR_CONTACT_ID_ALREADY_EXISTS));
-      g_error_free (error);
-    }
-  else
-    g_error ("Succeeded in adding two contacts with the same UID !");
+	if (!e_book_client_add_contact_sync (book_client, contact, NULL, NULL, &error)) {
+		g_assert (g_error_matches (error, E_BOOK_CLIENT_ERROR, E_BOOK_CLIENT_ERROR_CONTACT_ID_ALREADY_EXISTS));
+		g_error_free (error);
+	} else
+		g_error ("Succeeded in adding two contacts with the same UID !");
 
-  g_object_unref (contact);
+	g_object_unref (contact);
 }
 
 gint
