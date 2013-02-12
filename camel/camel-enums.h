GNOME/evolution-data-server

commit 623eb5fc7dd2b21fe6c23df0e2020b4797d1f83c

Document CamelMimeFilterToHTMLFlags.

camel/camel-enums.h
@@ -135,6 +135,31 @@ typedef enum { /*< flags >*/
 
 #define CAMEL_FOLDER_TYPE_MASK (63 << CAMEL_FOLDER_TYPE_BIT)
 
+/**
+ * CamelMimeFilterToHTMLFlags:
+ * @CAMEL_MIME_FILTER_TOHTML_PRE:
+ *     Enclose the content in <pre> ... </pre> tags.
+ * @CAMEL_MIME_FILTER_TOHTML_CONVERT_NL:
+ *     Convert newline characters to <br> tags.
+ * @CAMEL_MIME_FILTER_TOHTML_CONVERT_SPACES:
+ *     Convert space and tab characters to a non-breaking space (&nbsp;).
+ * @CAMEL_MIME_FILTER_TOHTML_CONVERT_URLS:
+ *     Convert recognized URLs to <a href="foo">foo</a>.
+ * @CAMEL_MIME_FILTER_TOHTML_MARK_CITATION:
+ *     Color quoted lines (lines beginning with '>').
+ * @CAMEL_MIME_FILTER_TOHTML_CONVERT_ADDRESSES:
+ *     Convert mailto: URLs to <a href="mailto:foo">mailto:foo</a>.
+ * @CAMEL_MIME_FILTER_TOHTML_ESCAPE_8BIT:
+ *     Convert 8-bit characters to escaped hexdecimal (&#nnn;).
+ * @CAMEL_MIME_FILTER_TOHTML_CITE:
+ *     Prefix each line with "> ".
+ * @CAMEL_MIME_FILTER_TOHTML_PRESERVE_8BIT:
+ *     This flag is not used by #CamelMimeFilterToHTML.
+ * @CAMEL_MIME_FILTER_TOHTML_FORMAT_FLOWED:
+ *     This flag is not used by #CamelMimeFilterToHTML.
+ *
+ * Flags for converting text/plain content into text/html.
+ **/
 typedef enum { /*< flags >*/
 	CAMEL_MIME_FILTER_TOHTML_PRE               = 1 << 0,
 	CAMEL_MIME_FILTER_TOHTML_CONVERT_NL        = 1 << 1,

commit aa055d935d4b55dc7ed91857871b9cb678d7a338

Fix a gtk-doc build break caused by commit 623eb5fc7dd2b21f
The HTML tags in gtk-doc comments should be properly escaped,
thus gtk-doc doesn't consider them as real HTML tags.
Thanks Fabien (fta) for the notice.

camel/camel-enums.h
@@ -135,32 +135,34 @@ typedef enum { /*< flags >*/
 
 #define CAMEL_FOLDER_TYPE_MASK (63 << CAMEL_FOLDER_TYPE_BIT)
 
+/* Note: The HTML elements are escaped in the doc comment intentionally,
+   to have them shown as expected in generated documentation. */
 /**
  * CamelMimeFilterToHTMLFlags:
  * @CAMEL_MIME_FILTER_TOHTML_PRE:
- *     Enclose the content in <pre> ... </pre> tags.
+ *     Enclose the content in &lt;pre&gt; ... &lt;/pre&gt; tags.
  * @CAMEL_MIME_FILTER_TOHTML_CONVERT_NL:
- *     Convert newline characters to <br> tags.
+ *     Convert newline characters to &lt;br&gt; tags.
  * @CAMEL_MIME_FILTER_TOHTML_CONVERT_SPACES:
- *     Convert space and tab characters to a non-breaking space (&nbsp;).
+ *     Convert space and tab characters to a non-breaking space (&amp;nbsp;).
  * @CAMEL_MIME_FILTER_TOHTML_CONVERT_URLS:
- *     Convert recognized URLs to <a href="foo">foo</a>.
+ *     Convert recognized URLs to &lt;a href="foo"&gt;foo&lt;/a&gt;.
  * @CAMEL_MIME_FILTER_TOHTML_MARK_CITATION:
- *     Color quoted lines (lines beginning with '>').
+ *     Color quoted lines (lines beginning with '&gt;').
  * @CAMEL_MIME_FILTER_TOHTML_CONVERT_ADDRESSES:
- *     Convert mailto: URLs to <a href="mailto:foo">mailto:foo</a>.
+ *     Convert mailto: URLs to &lt;a href="mailto:foo"&gt;mailto:foo&lt;/a&gt;.
  * @CAMEL_MIME_FILTER_TOHTML_ESCAPE_8BIT:
- *     Convert 8-bit characters to escaped hexdecimal (&#nnn;).
+ *     Convert 8-bit characters to escaped hexdecimal (&amp;#nnn;).
  * @CAMEL_MIME_FILTER_TOHTML_CITE:
- *     Prefix each line with "> ".
+ *     Prefix each line with "&gt; ".
  * @CAMEL_MIME_FILTER_TOHTML_PRESERVE_8BIT:
  *     This flag is not used by #CamelMimeFilterToHTML.
  * @CAMEL_MIME_FILTER_TOHTML_FORMAT_FLOWED:
  *     This flag is not used by #CamelMimeFilterToHTML.
  * @CAMEL_MIME_FILTER_TOHTML_QUOTE_CITATION:
- *     Group lines beginning with one or more '>' characters in
- *     <blockquote type="cite"> ... </blockquote> tags.  The tags
- *     are nested according to the number of '>' characters.
+ *     Group lines beginning with one or more '&gt;' characters in
+ *     &lt;blockquote type="cite"&gt; ... &lt;/blockquote&gt; tags. The tags
+ *     are nested according to the number of '&gt;' characters.
  *
  * Flags for converting text/plain content into text/html.
  **/