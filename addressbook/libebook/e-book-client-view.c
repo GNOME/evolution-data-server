ba04cac4ea79b8ab74cfb13384ff4722873fda94
Matthew Barnes <mbarnes@redhat.com>
2013-02-27 21:24:46 -0500

    EBookClientView: Don't block in dispose().

--- addressbook/libebook/e-book-client-view.c
+++ addressbook/libebook/e-book-client-view.c
@@ -531,6 +531,22 @@ book_client_view_complete_cb (EGdbusBookView *object,
 }
 
 static void
+book_client_view_dispose_cb (GObject *source_object,
+                             GAsyncResult *result,
+                             gpointer user_data)
+{
+	GError *error = NULL;
+
+	e_gdbus_book_view_call_dispose_finish (
+		G_DBUS_PROXY (source_object), result, &error);
+
+	if (error != NULL) {
+		g_warning ("%s: %s", G_STRFUNC, error->message);
+		g_error_free (error);
+	}
+}
+
+static void
 book_client_view_set_client (EBookClientView *view,
                              EBookClient *client)
 {
@@ -662,8 +678,6 @@ book_client_view_dispose (GObject *object)
 	}
 
 	if (priv->dbus_proxy != NULL) {
-		GError *error = NULL;
-
 		g_signal_handler_disconnect (
 			priv->dbus_proxy,
 			priv->objects_added_handler_id);
@@ -680,17 +694,11 @@ book_client_view_dispose (GObject *object)
 			priv->dbus_proxy,
 			priv->complete_handler_id);
 
-		e_gdbus_book_view_call_dispose_sync (
-			priv->dbus_proxy, NULL, &error);
-
-		if (error != NULL) {
-			g_dbus_error_strip_remote_error (error);
-			g_warning (
-				"Failed to dispose book view: %s",
-				error->message);
-			g_error_free (error);
-		}
-
+		/* Call D-Bus dispose() asynchronously
+		 * so we don't block this dispose() .*/
+		e_gdbus_book_view_call_dispose (
+			priv->dbus_proxy, NULL,
+			book_client_view_dispose_cb, NULL);
 		g_object_unref (priv->dbus_proxy);
 		priv->dbus_proxy = NULL;
 	}
