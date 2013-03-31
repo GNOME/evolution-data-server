88555fb8aa93950d2fdc7602bb335dbd9ec73bb1
Matthew Barnes <mbarnes@redhat.com>
2013-03-31 08:28:19 -0400

    EBookBackend: Remove CLIENT_BACKEND_PROPERTY_ONLINE definition.
    Backends should use the EBackend:online property directly.

--- addressbook/libedata-book/e-book-backend.h
+++ addressbook/libedata-book/e-book-backend.h
@@ -50,15 +50,6 @@
 	((obj), E_TYPE_BOOK_BACKEND, EBookBackendClass))
 
 /**
- * CLIENT_BACKEND_PROPERTY_ONLINE:
- *
- * FIXME: Document me.
- *
- * Since: 3.2
- **/
-#define CLIENT_BACKEND_PROPERTY_ONLINE			"online"
-
-/**
  * CLIENT_BACKEND_PROPERTY_READONLY:
  *
  * FIXME: Document me.
