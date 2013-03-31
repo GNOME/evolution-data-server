260e702cfd5984cee4d0c21c3d10e6f63b13bbe5
Matthew Barnes <mbarnes@redhat.com>
2013-03-31 08:56:05 -0400

    ECalBackend: Remove CLIENT_BACKEND_PROPERTY_READONLY definition.
    Backends should use the ECalBackend:writable property directly.

--- calendar/libedata-cal/e-cal-backend.h
+++ calendar/libedata-cal/e-cal-backend.h
@@ -48,15 +48,6 @@
 	((obj), E_TYPE_CAL_BACKEND, ECalBackendClass))
 
 /**
- * CLIENT_BACKEND_PROPERTY_READONLY:
- *
- * FIXME: Document me.
- *
- * Since: 3.2
- **/
-#define CLIENT_BACKEND_PROPERTY_READONLY		"readonly"
-
-/**
  * CLIENT_BACKEND_PROPERTY_CACHE_DIR:
  *
  * FIXME: Document me.
