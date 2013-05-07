b15225b7a7851ee90e33b741459cd73f7656e56f
Tristan Van Berkom <tristanvb@openismus.com>
2013-05-07 22:00:32 +0900

    Ported data migration tests to use the new ICU sorted data.
    Added migration tests also for fr_CA and de_DE locale, the
    de_DE locale expects a @collation=phonebook type of sort order
    but only the test-sqlite-cursor-posix-initial test which creates
    the shared addressbook defines it, so we also test that the
    collation rule is properly stored and introspected in the
    SQLite tables.

--- tests/libedata-book/test-sqlite-cursor-de-DE-migrated.c
+++ tests/libedata-book/test-sqlite-cursor-de-DE-migrated.c
@@ -6,7 +6,6 @@
 
 #include "data-test-utils.h"
 
-
 gint
 main (gint argc,
       gchar **argv)
@@ -19,15 +18,25 @@ main (gint argc,
 	g_test_init (&argc, &argv, NULL);
 
 	/* Ensure that the client and server get the same locale */
-	g_assert (g_setenv ("EDS_COLLATE", "en_US.UTF-8", TRUE));
+	g_assert (g_setenv ("EDS_COLLATE", "de_DE.UTF-8", TRUE));
 	g_assert (g_setenv ("MIGRATION_TEST_SOURCE_NAME", "migration-test-source", TRUE));
 
-	/* Try them all once in the normal base locale */
-	data = move_by_test_new ("/EbSdbCursor/Locale/en_US/Migrated", FALSE);
+	/* Here we rely on the initial addressbook book created by
+	 * test-sqlite-cursor-posix-initial, which created the addressbook
+	 * with the phonebook collation rule.
+	 *
+	 * Here we assert that the SQLite introspection works properly after
+	 * a migration, and the recorded SQLite collation names are used instead of the
+	 * parameters when opening the addressbook (since we pass FALSE here
+	 * to move_by_test_new(), we create our addressbook with no special collation rule).
+	 */
+	data = move_by_test_new ("/EbSdbCursor/Locale/de_DE/Migrated", FALSE);
+
+	/* Assert the whole set of contacts */
 	move_by_test_add_assertion (data, 5, 11, 1,  2,  5,  6);
-	move_by_test_add_assertion (data, 5, 4,  3,  7,  8,  15);
+	move_by_test_add_assertion (data, 5, 7,  8,  4,  3,  15);
 	move_by_test_add_assertion (data, 5, 17, 16, 18, 10, 14);
-	move_by_test_add_assertion (data, 5, 12, 13, 9,  19, 20);
+	move_by_test_add_assertion (data, 5, 12, 13, 9,  20, 19);
 	move_by_test_add (data, FALSE);
 
 	/* On this case, we are using the migrated addressbook, don't delete it first */
