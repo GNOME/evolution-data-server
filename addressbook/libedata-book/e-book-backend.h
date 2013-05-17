f44aa895cdecd97651dfa2a42717a012ed9607a5
Tristan Van Berkom <tristanvb@openismus.com>
2013-05-17 20:03:45 +0900

    EBookBackend added new ->set_locale() API and ->get_locale() API
    Use these to dynamically set the locale of a running addressbook, and also
    to fetch the currently configured locale of an addressbook at startup time

--- addressbook/libedata-book/e-book-backend.h
+++ addressbook/libedata-book/e-book-backend.h
@@ -200,6 +200,9 @@ struct _EBookBackendClass {
 			(*get_direct_book)	(EBookBackend *backend);
 	void		(*configure_direct)	(EBookBackend *backend,
 						 const gchar *config);
+	void            (*set_locale)           (EBookBackend *backend,
+						 const gchar  *locale);
+	const gchar    *(*get_locale)           (EBookBackend *backend);
 
 	/* Notification signals */
 	void		(*sync)			(EBookBackend *backend);
@@ -301,6 +304,9 @@ EDataBookDirect *
 void            e_book_backend_configure_direct (EBookBackend *backend, const gchar *config);
 
 void		e_book_backend_sync		(EBookBackend *backend);
+void            e_book_backend_set_locale       (EBookBackend *backend,
+						 const gchar  *locale);
+const gchar    *e_book_backend_get_locale       (EBookBackend *backend);
 
 /* protected functions for subclasses */
 void		e_book_backend_set_is_removed	(EBookBackend *backend,
