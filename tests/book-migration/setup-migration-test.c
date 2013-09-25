ca64872e132770264f59b49418f5304c8c89e832
Tristan Van Berkom <tristanvb@openismus.com>
2013-09-25 19:54:07 +0200

    Bug 709198 - Add regression test for contacts database migrations
    The migration tests come with a few components:
    
      o tests/book-migration/vcards/*.vcf:
        The vcards which the test case expects to find in the migrated
        databases.
    
      o tests/book-migration/db/[various versions]/[File backend files]:
        Various sandboxes created from the vcards, each one created
        with a different stable version of EDS
    
      o tests/book-migration/test-migration.c:
        The actual test, this automatically loops over the available
        test directories and runs the migration suite for each version
    
      o tests/book-migration/setup-migration-test.c:
        A simple program used to inject the test contacts into EDS,
        this program can be used to generate the test sandbox from
        EDS version 3.0 onward.
    
      o The makefile also contains a 'setup-migration' target which
        can be used to generate a new repository automatically in
        the tests/book-migration/db/ directory with the current version,
        this can only be used to generate the sandbox from 3.10 and
        above.

---
+++ tests/book-migration/setup-migration-test.c
@@ -0,0 +1,360 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * Copyright (C) 2013 Intel Corporation
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU Lesser General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ * Authors: Tristan Van Berkom <tristanvb@openismus.com>
+ */
+
+#include <glib.h>
+
+#include <libedataserver/eds-version.h>
+
+#if EDS_CHECK_VERSION(3,6,0)
+#  include <libebook/libebook.h>
+/* In 3.10, we support the --use-test-sandbox option */
+#  if EDS_CHECK_VERSION(3,10,0)
+#    include "client-test-utils.h"
+#    include "e-test-server-utils.h"
+#  endif
+#else
+#  include <libebook/e-contact.h>
+#  include <libebook/e-book.h>
+#endif
+
+#if EDS_CHECK_VERSION(3,6,0)
+typedef EBookClient Book;
+#else
+typedef EBook Book;
+#endif
+
+static Book *create_book  (const gchar *book_id);
+static void  add_contacts (Book *book, GSList *contacts);
+
+static gchar    *book_id = NULL;
+static gchar    *contacts_directory = NULL;
+static gboolean  test_sandbox = FALSE;
+
+static GOptionEntry option_entries[] = {
+	{"book-id", 'b', 0, G_OPTION_ARG_STRING, &book_id,
+	 "The book identifier string", NULL },
+	{"contacts-directory", 'd', 0, G_OPTION_ARG_FILENAME, &contacts_directory,
+	 "The directory from where to read the contact files", NULL },
+	{"use-test-sandbox", 't', 0, G_OPTION_ARG_NONE, &test_sandbox,
+	 "Whether to use the test case sandbox to create the test book "
+	 "(Only available after EDS 3.10)", NULL },
+	{ NULL }
+};
+
+/********************************************************
+ *            Loading contacts from directory           *
+ ********************************************************/
+static EContact *
+contact_from_file (const gchar *vcard_file)
+{
+	EContact *contact;
+	GError *error;
+	gchar *vcard = NULL;
+
+	if (!g_file_get_contents (vcard_file, &vcard, NULL, &error))
+		g_error ("Failed to load vcard: %s", error->message);
+
+	contact = e_contact_new_from_vcard (vcard);
+	g_free (vcard);
+
+	return contact;
+}
+
+static GSList *
+load_contacts (const gchar *vcard_directory)
+{
+	GDir *dir;
+	GError *error = NULL;
+	const gchar *filename;
+	GSList *contacts = NULL;
+
+	dir = g_dir_open (vcard_directory, 0, &error);
+	if (!dir)
+		g_error ("Failed to open vcard directory '%s': %s", vcard_directory, error->message);
+
+	while ((filename = g_dir_read_name (dir)) != NULL) {
+
+		if (g_str_has_suffix (filename, ".vcf")) {
+			gchar *fullpath = g_build_filename (vcard_directory, filename, NULL);
+			EContact *contact;
+
+			contact = contact_from_file (fullpath);
+			contacts = g_slist_prepend (contacts, contact);
+
+			g_free (fullpath);
+		}
+	}
+
+	g_dir_close (dir);
+
+	return g_slist_reverse (contacts);
+}
+
+/********************************************************
+ *                   Creating the Book                  *
+ ********************************************************/
+#if EDS_CHECK_VERSION(3,6,0)
+
+typedef struct {
+	GMainLoop *loop;
+	const gchar *book_id;
+	EBookClient *book;
+} SourceAddedData;
+
+static void
+source_added (ESourceRegistry *registry,
+	      ESource *source,
+	      gpointer data)
+{
+	SourceAddedData *added_data = (SourceAddedData *)data;
+	GError *error = NULL;
+
+	if (g_strcmp0 (e_source_get_uid (source), added_data->book_id) != 0)
+		return;
+
+	/* Open the address book */
+#if EDS_CHECK_VERSION(3,8,0)
+	added_data->book = (EBookClient *)e_book_client_connect_sync (source, NULL, &error);
+#else
+	/* With 3.6 it's a bit more tricky */
+	added_data->book = e_book_client_new (source, &error);
+	if (added_data->book &&
+	    !e_client_open_sync (E_CLIENT (added_data->book), FALSE, NULL, &error))
+		g_error ("Failed to open addressbook: %s", error->message);
+#endif
+
+	if (!added_data->book)
+		g_error ("Failed to create addressbook: %s", error->message);
+
+	if (added_data->loop)
+		g_main_loop_quit (added_data->loop);
+}
+
+static gboolean
+create_source_timeout (gpointer user_data)
+{
+	g_error ("Timed out while waiting for ESource creation from the registry");
+
+	return FALSE;
+}
+
+static EBookClient *
+create_book (const gchar *book_id)
+{
+	ESourceRegistry *registry;
+	ESource *scratch;
+	ESourceBackend *backend = NULL;
+	GError  *error = NULL;
+	SourceAddedData data = { NULL, NULL, NULL };
+
+	g_return_val_if_fail (book_id != NULL, NULL);
+
+	data.loop = g_main_loop_new (NULL, FALSE);
+
+	registry = e_source_registry_new_sync (NULL, &error);
+	if (!registry)
+		g_error ("Unable to create the registry: %s", error->message);
+
+	/* Listen to the registry for our added source */
+	data.book_id = book_id;
+	g_signal_connect (registry, "source-added",
+			  G_CALLBACK (source_added), &data);
+
+	/* Now create a scratch source for our addressbook */
+	scratch = e_source_new_with_uid (book_id, NULL, &error);
+	if (!scratch)
+		g_error ("Failed to create scratch source: %s", error->message);
+
+	/* Ensure the new ESource will be a local addressbook source */
+	backend = e_source_get_extension (scratch, E_SOURCE_EXTENSION_ADDRESS_BOOK);
+	e_source_backend_set_backend_name (backend, "local");
+
+	/* Commit the source to the registry */
+	if (!e_source_registry_commit_source_sync (registry, scratch, NULL, &error))
+		g_error ("Unable to add new addressbook source to the registry: %s", error->message);
+
+	g_object_unref (scratch);
+
+	if (data.book == NULL) {
+		g_timeout_add (20 * 1000, create_source_timeout, NULL);
+		g_main_loop_run (data.loop);
+
+		/* By now we aborted or we have an addressbook created */
+		g_assert (data.book != NULL);
+	}
+
+	g_main_loop_unref (data.loop);
+	g_object_unref (registry);
+
+	return data.book;
+}
+
+#else
+
+static EBook *
+create_book (const gchar *book_id)
+{
+	EBook *book;
+	GError *error = NULL;
+
+	book = e_book_new_from_uri (book_id, &error);
+
+	if (!book)
+		g_error ("Error creating book: %s", error->message);
+
+        if (!e_book_open (book, FALSE, &error))
+		g_error ("Error opening book: %s", error->message);
+
+	return book;
+}
+
+#endif
+
+
+/********************************************************
+ *                   Adding the Contacts                *
+ ********************************************************/
+#if EDS_CHECK_VERSION(3,6,0)
+
+static void
+add_contacts (Book   *book,
+	      GSList *contacts)
+{
+	GError *error = NULL;
+
+	if (!e_book_client_add_contacts_sync (book, contacts, NULL, NULL, &error))
+		g_error ("Failed to add contacts: %s", error->message);
+}
+
+#else
+
+static void
+add_contacts (Book   *book,
+	      GSList *contacts)
+{
+	GError *error = NULL;
+	GSList *l;
+
+	for (l = contacts; l; l = l->next) {
+		EContact *contact = l->data;
+
+		if (!e_book_add_contact (book, contact, &error))
+			g_error ("Failed to add a contact: %s", error->message);
+	}
+}
+
+#endif
+
+/* Support running this in a test case in 3.10 and later */
+#if EDS_CHECK_VERSION(3,10,0)
+
+static ETestServerClosure book_closure = {
+	E_TEST_SERVER_ADDRESS_BOOK,
+	NULL, /* Source customization function */
+	0,    /* Calendar Type */
+	TRUE, /* Keep the working sandbox after the test, don't remove it */
+	NULL, /* Destroy Notify function */
+};
+
+static void
+setup_migration_setup (ETestServerFixture *fixture,
+		       gconstpointer       user_data)
+{
+	fixture->source_name = g_strdup_printf ("%d.%d", EDS_MAJOR_VERSION, EDS_MINOR_VERSION);
+	e_test_server_utils_setup (fixture, user_data);
+}
+
+static void
+setup_migration_run (ETestServerFixture *fixture,
+		     gconstpointer       user_data)
+{
+	EBookClient *book_client;
+	GSList      *contacts;
+
+	contacts = load_contacts (contacts_directory);
+
+	book_client = E_TEST_SERVER_UTILS_SERVICE (fixture, EBookClient);
+	add_contacts (book_client, contacts);
+	g_slist_free_full (contacts, g_object_unref);
+}
+
+#endif
+
+/********************************************************
+ *                        main()                        *
+ ********************************************************/
+gint
+main (gint argc, gchar *argv[])
+{
+	GOptionContext *option_context;
+	GOptionGroup *option_group;
+	GError *error = NULL;
+	GSList *contacts;
+	Book *book;
+
+	option_context = g_option_context_new (NULL);
+	g_option_context_set_summary (option_context,
+				      "Populate a database for migration tests.");
+
+	option_group = g_option_group_new ("setup migration test",
+					   "Setup Migration options",
+					   "Setup Migration options", NULL, NULL);
+	g_option_group_add_entries (option_group, option_entries);
+	g_option_context_set_main_group (option_context, option_group);
+
+	if (!g_option_context_parse (option_context, &argc, &argv, &error))
+		g_error ("Failed to parse program arguments: %s", error->message);
+
+	if (!book_id || !contacts_directory)
+		g_error ("Must provide the book identifier and contacts directory\n%s",
+			 g_option_context_get_help (option_context, TRUE, NULL));
+
+
+	if (test_sandbox) {
+
+#if EDS_CHECK_VERSION(3,10,0)
+		g_test_init (&argc, &argv, NULL);
+		g_test_add ("/SettingUpMigrationTest",
+			    ETestServerFixture,
+			    &book_closure,
+			    setup_migration_setup,
+			    setup_migration_run,
+			    e_test_server_utils_teardown);
+
+		return e_test_server_utils_run ();
+#else
+		g_error ("Requested sandboxed setup but that is not available until EDS 3.10, current version is %d.%d",
+			 EDS_MAJOR_VERSION, EDS_MINOR_VERSION);
+#endif
+	} else {
+
+		contacts = load_contacts (contacts_directory);
+
+		book = create_book (book_id);
+		add_contacts (book, contacts);
+
+		g_object_unref (book);
+
+		g_slist_free_full (contacts, g_object_unref);
+	}
+
+	return 0;
+}
---
+++ tests/book-migration/test-migration.c
@@ -0,0 +1,374 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * Copyright (C) 2013 Intel Corporation
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU Lesser General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ * Authors: Tristan Van Berkom <tristanvb@openismus.com>
+ */
+
+#include <config.h>
+
+#include <stdlib.h>
+#include <libebook/libebook.h>
+
+#include "client-test-utils.h"
+#include "e-test-server-utils.h"
+
+typedef struct {
+	ETestServerFixture parent_fixture;
+} MigrationFixture;
+
+typedef struct {
+	ETestServerClosure  parent;
+	gchar              *version;
+} MigrationClosure;
+
+typedef void ( *MigrationTestFunc) (MigrationFixture *fixture,
+				    gconstpointer     user_data);
+
+static const gchar *arbitrary_vcard = 
+	"BEGIN:VCARD\n"
+	"UID:arbitrary-vcard\n"
+	"FN:Bobby Brown\n"
+	"TEL;HOME:+9999999\n"
+	"EMAIL;TYPE=work:bobby@brown.org\n"
+	"EMAIL;TYPE=home,work:bobby@brown.com\n"
+	"END:VCARD\n";
+
+/***********************************************************
+ *                         Fixture                         *
+ ***********************************************************/
+static void
+setup_migration_sandbox (const gchar *version)
+{
+	gchar *dest_dir, *dest_bdb, *dest;
+	gchar *src_bdb, *src;
+	GFile *src_file, *dest_file;
+	GError *error = NULL;
+
+	dest_dir  = g_build_filename (EDS_TEST_WORK_DIR, "evolution", "addressbook", version, NULL);
+	dest_bdb  = g_build_filename (dest_dir, "addressbook.db", NULL);
+	dest      = g_build_filename (dest_dir, "contacts.db", NULL);
+
+	src_bdb     = g_build_filename (EDS_TEST_BUILT_BOOKS, version, "addressbook.db", NULL);
+	src         = g_build_filename (EDS_TEST_SQLITE_BOOKS, version, "contacts.db", NULL);
+
+	/* Create the directory for the database files */
+	g_assert (g_mkdir_with_parents (dest_dir, 0755) == 0);
+
+	/* If there is a BDB for this version, copy it over */
+	if (g_file_test (src_bdb, G_FILE_TEST_IS_REGULAR)) {
+		src_file = g_file_new_for_path (src_bdb);
+		dest_file = g_file_new_for_path (dest_bdb);
+
+		if (!g_file_copy (src_file, dest_file,
+				  G_FILE_COPY_OVERWRITE |
+				  G_FILE_COPY_TARGET_DEFAULT_PERMS,
+				  NULL, NULL, NULL, &error))
+			g_error ("Failed to setup sandbox for %s migration test: %s",
+				 version, error->message);
+
+		g_object_unref (src_file);
+		g_object_unref (dest_file);
+	}
+
+	/* Setup the contacts.db for migration */
+	if (g_file_test (src, G_FILE_TEST_IS_REGULAR)) {
+		src_file = g_file_new_for_path (src);
+		dest_file = g_file_new_for_path (dest);
+
+		if (!g_file_copy (src_file, dest_file,
+				  G_FILE_COPY_OVERWRITE |
+				  G_FILE_COPY_TARGET_DEFAULT_PERMS,
+				  NULL, NULL, NULL, &error))
+			g_error ("Failed to setup sandbox for %s migration test: %s",
+				 version, error->message);
+
+		g_object_unref (src_file);
+		g_object_unref (dest_file);
+	}
+
+	g_free (dest_dir);
+	g_free (dest_bdb);
+	g_free (dest);
+
+	g_free (src_bdb);
+	g_free (src);
+}
+
+static void
+migration_fixture_setup (MigrationFixture *fixture,
+			 gconstpointer     user_data)
+{
+	ETestServerFixture *parent = (ETestServerFixture *)fixture;
+	MigrationClosure   *closure = (MigrationClosure *)user_data;
+
+	parent->source_name = g_strdup (closure->version);
+
+	setup_migration_sandbox (closure->version);
+
+	e_test_server_utils_setup ((ETestServerFixture *)parent, user_data);
+}
+
+static void
+migration_fixture_teardown (MigrationFixture *fixture,
+			    gconstpointer     user_data)
+{
+	e_test_server_utils_teardown ((ETestServerFixture *)fixture, user_data);
+}
+
+
+/***********************************************************
+ *                          Tests                          *
+ ***********************************************************/
+static void
+test_open (MigrationFixture *fixture,
+	   gconstpointer     user_data)
+{
+	
+}
+
+static void
+test_fetch_contacts (MigrationFixture *fixture,
+		     gconstpointer     user_data)
+{
+	EBookClient *book_client;
+	GSList *contacts = NULL;
+	GError *error = NULL;
+
+	book_client = E_TEST_SERVER_UTILS_SERVICE (fixture, EBookClient);
+
+	if (!e_book_client_get_contacts_sync (book_client,
+					      "",
+					      &contacts,
+					      NULL, &error))
+		g_error ("Failed to fetch contacts: %s", error->message);
+
+	/* Assert some more things related to the actually expected contacts here... */
+	g_assert_cmpint (g_slist_length (contacts), ==, 20);
+
+	g_slist_free_full (contacts, g_object_unref);
+}
+
+static void
+test_add_remove_contact (MigrationFixture *fixture,
+			 gconstpointer     user_data)
+{
+	EBookClient *book_client;
+	EContact *contact = NULL;
+	GError *error = NULL;
+
+	book_client = E_TEST_SERVER_UTILS_SERVICE (fixture, EBookClient);
+
+	contact = e_contact_new_from_vcard (arbitrary_vcard);
+
+	if (!e_book_client_add_contact_sync (book_client, contact, NULL, NULL, &error))
+		g_error ("Failed to add contact: %s", error->message);
+
+	if (!e_book_client_remove_contact_sync (book_client, contact, NULL, &error))
+		g_error ("Failed to remove contact: %s", error->message);
+
+	g_object_unref (contact);
+}
+
+static GSList *
+test_query (EBookClient *book_client,
+	    gint         expected_results,
+	    EBookQuery  *query)
+{
+	GSList *contacts = NULL;
+	GError *error = NULL;
+	gchar *sexp;
+
+	sexp = e_book_query_to_string (query);
+
+	if (!e_book_client_get_contacts_sync (book_client, sexp,
+					      &contacts, NULL, &error))
+		g_error ("Failed to fetch contacts: %s", error->message);
+
+	g_free (sexp);
+	e_book_query_unref (query);
+
+	g_assert_cmpint (g_slist_length (contacts), ==, expected_results);
+
+	return contacts;
+}
+
+static void
+test_query_email (MigrationFixture *fixture,
+		  gconstpointer     user_data)
+{
+	EBookClient *book_client;
+	GSList *contacts = NULL;
+
+	book_client = E_TEST_SERVER_UTILS_SERVICE (fixture, EBookClient);
+
+	contacts = test_query (book_client, 13,
+			       e_book_query_field_test (
+			               E_CONTACT_EMAIL,
+				       E_BOOK_QUERY_ENDS_WITH,
+				       ".com"));
+
+	g_slist_free_full (contacts, g_object_unref);
+}
+
+static void
+test_query_name (MigrationFixture *fixture,
+		  gconstpointer     user_data)
+{
+	EBookClient *book_client;
+	GSList *contacts = NULL;
+
+	book_client = E_TEST_SERVER_UTILS_SERVICE (fixture, EBookClient);
+
+	contacts = test_query (book_client, 4,
+			       e_book_query_field_test (
+			               E_CONTACT_FULL_NAME,
+				       E_BOOK_QUERY_CONTAINS,
+				       "cote"));
+
+	g_slist_free_full (contacts, g_object_unref);
+}
+
+static void
+test_query_phone (MigrationFixture *fixture,
+		  gconstpointer     user_data)
+{
+	EBookClient *book_client;
+	GSList *contacts = NULL;
+
+	book_client = E_TEST_SERVER_UTILS_SERVICE (fixture, EBookClient);
+
+	contacts = test_query (book_client, 4,
+			       e_book_query_field_test (
+			               E_CONTACT_TEL,
+				       E_BOOK_QUERY_CONTAINS,
+				       "221"));
+
+	g_slist_free_full (contacts, g_object_unref);
+}
+
+/***********************************************************
+ *                          Main                           *
+ ***********************************************************/
+static GList *
+list_migration_sandboxes (void)
+{
+	GDir *dir;
+	GError *error = NULL;
+	const gchar *filename;
+	GList *sandboxes = NULL;
+
+	dir = g_dir_open (EDS_TEST_SQLITE_BOOKS, 0, &error);
+	if (!dir)
+		g_error ("Failed to open migration sandbox directory '%s': %s",
+			 EDS_TEST_SQLITE_BOOKS, error->message);
+
+	while ((filename = g_dir_read_name (dir)) != NULL) {
+
+		gchar *fullpath = g_build_filename (EDS_TEST_SQLITE_BOOKS, filename, NULL);
+
+		if (g_file_test (fullpath, G_FILE_TEST_IS_DIR)) {
+
+#if defined (TEST_VERSIONS_WITH_BDB)
+			sandboxes = g_list_prepend (sandboxes, g_strdup (filename));
+#else
+			/* We allow compilation of EDS on a system without the db_load utility, if this
+			 * is the case then we skip the migration tests from versions of EDS where we
+			 * used Berkeley DB
+			 */
+			gchar *old_bdb = g_build_filename (EDS_TEST_BOOKS, filename, "addressbook.dump", NULL);
+
+			if (!g_file_test (old_bdb, G_FILE_TEST_EXISTS))
+				sandboxes = g_list_prepend (sandboxes, g_strdup (filename));
+
+			g_free (old_bdb);
+#endif
+		}
+
+		g_free (fullpath);
+	}
+
+	g_dir_close (dir);
+
+	return sandboxes;
+}
+
+static void
+migration_closure_free (MigrationClosure *closure)
+{
+	g_free (closure->version);
+	g_slice_free (MigrationClosure, closure);
+}
+
+static void
+add_test (const gchar *version,
+	  const gchar *test_name,
+	  MigrationTestFunc test_func)
+{
+	MigrationClosure *closure;
+	gchar *path;
+
+	closure = g_slice_new0 (MigrationClosure);
+
+	closure->parent.type = E_TEST_SERVER_ADDRESS_BOOK;
+	closure->parent.destroy_closure_func = (GDestroyNotify)migration_closure_free;
+	closure->parent.keep_work_directory = TRUE;
+	closure->version = g_strdup (version);
+
+	path = g_strdup_printf ("/Migration/From-%s/%s",
+				version, test_name);
+
+	g_test_add (path, MigrationFixture, closure,
+		    migration_fixture_setup,
+		    test_func,
+		    migration_fixture_teardown);
+
+	g_free (path);
+}
+
+gint
+main (gint argc,
+      gchar **argv)
+{
+	GList *sandboxes, *l;
+	gint ret;
+
+#if !GLIB_CHECK_VERSION (2, 35, 1)
+	g_type_init ();
+#endif
+	g_test_init (&argc, &argv, NULL);
+	g_test_bug_base ("http://bugzilla.gnome.org/");
+
+	sandboxes = list_migration_sandboxes ();
+
+	for (l = sandboxes; l; l = l->next) {
+		gchar *version = l->data;
+
+		add_test (version, "Open", test_open);
+		add_test (version, "FetchContacts", test_fetch_contacts);
+		add_test (version, "AddRemoveContact", test_add_remove_contact);
+		add_test (version, "Query/FullName", test_query_name);
+		add_test (version, "Query/Phone", test_query_phone);
+		add_test (version, "Query/Email", test_query_email);
+	}
+
+	g_list_free_full (sandboxes, g_free);
+
+	ret = e_test_server_utils_run ();
+
+	return ret;
+}
