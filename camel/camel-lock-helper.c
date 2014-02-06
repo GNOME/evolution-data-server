0ca71c0ec12b6fcc83ed2d828fba586e356ff588
Milan Crha <mcrha@redhat.com>
2014-02-06 18:21:17 +0100

    Bug #720751 - camel_tag_set() doesn't copy tag's name properly

--- camel/camel-lock-helper.c
+++ camel/camel-lock-helper.c
@@ -176,7 +176,7 @@ lock_path (const gchar *path,
 		info->uid = lock_real_uid;
 	}
 
-	g_strlcpy (info->path, path, sizeof (info->path));
+	g_strlcpy (info->path, path, strlen (path) + 1);
 	info->id = lock_id;
 	info->depth = 1;
 	info->next = lock_info_list;
