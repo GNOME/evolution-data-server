GNOME/evolution-data-server

commit 69e94310c5dc8a55ff311f86e3fc40b59ea6fba3

Addressbook: Flushing DBus properties after setting new locale.
DBus signals about change of locale property for given address book are
deferred until all address books will update their locale.
Depending on the number of address books and their size delay between
changing address book's locale property and receiving signal about that
takes about one or two minutes.
Flushing DBus properties after changing locale property of address book
causes immediate emission of DBus signal.

addressbook/libedata-book/e-data-book.c
@@ -2162,9 +2162,12 @@ e_data_book_set_locale (EDataBook *book,
 	success = e_book_backend_set_locale (
 		backend, locale, cancellable, error);
 
-	if (success)
+	if (success) {
 		e_dbus_address_book_set_locale (
 			book->priv->dbus_interface, locale);
+		g_dbus_interface_skeleton_flush (
+			book->priv->dbus_interface);
+	}
 
 	g_object_unref (backend);
 

commit 90cb2c0bab0de3bb5e729bc578d178cb221ba46b

Fix a compiler warning caused by commit 69e94310 for bug #724626

addressbook/libedata-book/e-data-book.c
@@ -2166,7 +2166,7 @@ e_data_book_set_locale (EDataBook *book,
 		e_dbus_address_book_set_locale (
 			book->priv->dbus_interface, locale);
 		g_dbus_interface_skeleton_flush (
-			book->priv->dbus_interface);
+			G_DBUS_INTERFACE_SKELETON (book->priv->dbus_interface));
 	}
 
 	g_object_unref (backend);