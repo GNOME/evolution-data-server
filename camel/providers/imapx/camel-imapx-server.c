94c9a1f27c42fb6b304c40b727b46463b8b246fe
Milan Crha <mcrha@redhat.com>
2015-03-12 08:39:13 +0100

    [IMAPx] IDLE call timeouts for STARTTLS connections
    The STARTTLS connection uses GTLSConnection, which is a wrapper
    on top of a real (socket) connection. This GTLSConnection is not
    a descendant of GSocketConnection, thus the timeout was never changed
    in imapx_server_set_connection_timeout().

--- camel/providers/imapx/camel-imapx-server.c
+++ camel/providers/imapx/camel-imapx-server.c
@@ -1220,14 +1220,28 @@ imapx_server_set_connection_timeout (GIOStream *connection,
 	GSocket *socket;
 	gint previous_timeout = -1;
 
-	if (!G_IS_SOCKET_CONNECTION (connection))
+	if (G_IS_TLS_CONNECTION (connection)) {
+		GIOStream *base_io_stream = NULL;
+
+		g_object_get (G_OBJECT (connection), "base-io-stream", &base_io_stream, NULL);
+
+		connection = base_io_stream;
+	} else {
+		g_object_ref (connection);
+	}
+
+	if (!G_IS_SOCKET_CONNECTION (connection)) {
+		g_clear_object (&connection);
 		return previous_timeout;
+	}
 
 	socket = g_socket_connection_get_socket (G_SOCKET_CONNECTION (connection));
 	if (socket) {
 		previous_timeout = g_socket_get_timeout (socket);
 		g_socket_set_timeout (socket, timeout_seconds);
 	}
+
+	g_clear_object (&connection);
 
 	return previous_timeout;
 }
