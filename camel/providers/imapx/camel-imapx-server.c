3cd6ddc4e0b8f12f878e4d51f29cd180106f3228
Milan Crha <mcrha@redhat.com>
2015-08-07 13:43:44 +0200

    [IMAPx] imapx_server_inactivity_thread: Turn runtime warnings into debug messages

--- camel/providers/imapx/camel-imapx-server.c
+++ camel/providers/imapx/camel-imapx-server.c
@@ -652,13 +652,14 @@ imapx_server_inactivity_thread (gpointer user_data)
 	if (imapx_in_idle (is)) {
 		/* Stop and restart the IDLE command. */
 		if (!camel_imapx_server_schedule_idle_sync (is, NULL, is->priv->cancellable, &local_error) &&
-		    !g_error_matches (local_error, G_IO_ERROR, G_IO_ERROR_CANCELLED)) {
-			g_warning ("%s: Failed to restart IDLE: %s", G_STRFUNC, local_error ? local_error->message : "Unknown error");
-		}
+		    !g_error_matches (local_error, G_IO_ERROR, G_IO_ERROR_CANCELLED))
+			camel_imapx_debug (io, camel_imapx_server_get_tagprefix (is),
+				"%s: Failed to restart IDLE: %s\n", G_STRFUNC, local_error ? local_error->message : "Unknown error");
 	} else {
 		if (!camel_imapx_server_noop_sync (is, NULL, is->priv->cancellable, &local_error) &&
 		    !g_error_matches (local_error, G_IO_ERROR, G_IO_ERROR_CANCELLED))
-			g_warning ("%s: Failed to issue NOOP: %s", G_STRFUNC, local_error ? local_error->message : "Unknown error");
+			camel_imapx_debug (io, camel_imapx_server_get_tagprefix (is),
+				"%s: Failed to issue NOOP: %s\n", G_STRFUNC, local_error ? local_error->message : "Unknown error");
 	}
 
 	g_clear_error (&local_error);
