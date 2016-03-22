d85e809789352db5829b7840936cdd75a6ff989c
Milan Crha <mcrha@redhat.com>
2016-03-22 15:28:46 +0100

    Bug 763670 - [IMAPx] Silently ignore G_TLS_ERROR_EOF

--- camel/providers/imapx/camel-imapx-conn-manager.c
+++ camel/providers/imapx/camel-imapx-conn-manager.c
@@ -1281,6 +1281,7 @@ camel_imapx_conn_manager_run_job_sync (CamelIMAPXConnManager *conn_man,
 
 				if (!local_error ||
 				    g_error_matches (local_error, G_TLS_ERROR, G_TLS_ERROR_MISC) ||
+				    g_error_matches (local_error, G_TLS_ERROR, G_TLS_ERROR_EOF) ||
 				    g_error_matches (local_error, G_IO_ERROR, G_IO_ERROR_CLOSED)) {
 					GError *tmp = local_error;
 
