f92c784ea989a5fa364640b14dc27f0cac2f226d
Milan Crha <mcrha@redhat.com>
2016-10-13 11:31:56 +0200

    Bug 772347 - [IMAPx] NOTIFY responses stuck in stream until read from it

--- src/camel/providers/imapx/camel-imapx-server.c
+++ src/camel/providers/imapx/camel-imapx-server.c
@@ -6254,23 +6254,20 @@ imapx_server_run_idle_thread_cb (gpointer user_data)
 gboolean
 camel_imapx_server_can_use_idle (CamelIMAPXServer *is)
 {
-	gboolean use_idle = FALSE;
+	gboolean use_idle;
+	CamelIMAPXSettings *settings;
 
 	g_mutex_lock (&is->priv->stream_lock);
 
-	/* No need for IDLE if the server supports NOTIFY. */
-	if (CAMEL_IMAPX_HAVE_CAPABILITY (is->priv->cinfo, NOTIFY)) {
-		g_mutex_unlock (&is->priv->stream_lock);
+	settings = camel_imapx_server_ref_settings (is);
+	use_idle = camel_imapx_settings_get_use_idle (settings);
+	g_object_unref (settings);
 
-		return FALSE;
-	}
-
-	if (CAMEL_IMAPX_HAVE_CAPABILITY (is->priv->cinfo, IDLE)) {
-		CamelIMAPXSettings *settings;
-
-		settings = camel_imapx_server_ref_settings (is);
-		use_idle = camel_imapx_settings_get_use_idle (settings);
-		g_object_unref (settings);
+	/* Run IDLE if the server supports NOTIFY, to have
+	   a constant read on the stream, thus to be notified. */
+	if (!CAMEL_IMAPX_HAVE_CAPABILITY (is->priv->cinfo, NOTIFY) &&
+	    !CAMEL_IMAPX_HAVE_CAPABILITY (is->priv->cinfo, IDLE)) {
+		use_idle = FALSE;
 	}
 
 	g_mutex_unlock (&is->priv->stream_lock);
