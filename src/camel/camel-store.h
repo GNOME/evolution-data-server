9b5039e9d2593268c068df879a30a0f65be4b679
Corentin Noël <corentin@elementary.io>
2017-08-01 07:00:58 +0200

    Bug 785629 - Document the CamelStoreGetFolderFlags

--- src/camel/camel-store.h
+++ src/camel/camel-store.h
@@ -125,15 +125,23 @@ typedef struct _CamelStore CamelStore;
 typedef struct _CamelStoreClass CamelStoreClass;
 typedef struct _CamelStorePrivate CamelStorePrivate;
 
-/* open mode for folder */
+/**
+ * CamelStoreGetFolderFlags:
+ * @CAMEL_STORE_FOLDER_NONE: no flags
+ * @CAMEL_STORE_FOLDER_CREATE: create the folder
+ * @CAMEL_STORE_FOLDER_EXCL: deprecated, not honored
+ * @CAMEL_STORE_FOLDER_BODY_INDEX: save the body index
+ * @CAMEL_STORE_FOLDER_PRIVATE: a private folder that should not show up in
+ *  unmatched, folder info's, etc.
+ *
+ * Open mode for folder.
+ */
 typedef enum {
+	CAMEL_STORE_FOLDER_NONE = 0,
 	CAMEL_STORE_FOLDER_CREATE = 1 << 0,
-	CAMEL_STORE_FOLDER_EXCL = 1 << 1,       /* deprecated, not honored */
+	CAMEL_STORE_FOLDER_EXCL = 1 << 1,
 	CAMEL_STORE_FOLDER_BODY_INDEX = 1 << 2,
-	CAMEL_STORE_FOLDER_PRIVATE    = 1 << 3  /* a private folder that
-                                                   should not show up in
-                                                   unmatched, folder
-                                                   info's, etc. */
+	CAMEL_STORE_FOLDER_PRIVATE    = 1 << 3
 } CamelStoreGetFolderFlags;
 
 struct _CamelStore {
