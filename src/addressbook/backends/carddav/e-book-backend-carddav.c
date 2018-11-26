45866245fab2bb4f1a3fda86027df99756f64bd8
Milan Crha <mcrha@redhat.com>
2018-11-26 12:07:42 +0100

    [CalDAV/CardDAV] Can add component to local cache with incomplete info
    When a component/contact is loaded from the server when it's not part
    of the local cache (which can happen for example with meeting invitations),
    then the fetched object was stored without the href in the local cache,
    which made it impossible to edit the object later (it resulted
    in an "Invalid Object" error).

--- src/addressbook/backends/carddav/e-book-backend-carddav.c
+++ src/addressbook/backends/carddav/e-book-backend-carddav.c
@@ -1038,6 +1038,8 @@ ebb_carddav_load_contact_sync (EBookMetaBackend *meta_backend,
 				g_propagate_error (&local_error, EDB_ERROR_EX (E_DATA_BOOK_STATUS_OTHER_ERROR, _("Server didn’t return object’s ETag")));
 			else
 				g_propagate_error (&local_error, EDB_ERROR_EX (E_DATA_BOOK_STATUS_OTHER_ERROR, _("Received object is not a valid vCard")));
+		} else if (out_extra) {
+			*out_extra = g_strdup (href);
 		}
 	}
 
