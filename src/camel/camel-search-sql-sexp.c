8fcaaf1fac543634fc4b90252ef3c21262f0783a
Дилян Палаузов <git-dpa@aegee.org>
2019-07-07 15:43:10 +0000

    camel-search-sql-sexp.c:func_and() memory leak
    When string->len == 4, the value of string->str was leaked.

--- src/camel/camel-search-sql-sexp.c
+++ src/camel/camel-search-sql-sexp.c
@@ -80,12 +80,11 @@ func_and (CamelSExp *f,
           gpointer data)
 {
 	CamelSExpResult *r, *r1;
-	GString *string;
+	GString *string = g_string_new ("( ");
 	gint i;
 
 	d (printf ("executing and: %d", argc));
 
-	string = g_string_new ("( ");
 	for (i = 0; i < argc; i++) {
 		r1 = camel_sexp_term_eval (f, argv[i]);
 
@@ -101,10 +100,8 @@ func_and (CamelSExp *f,
 	r = camel_sexp_result_new (f, CAMEL_SEXP_RES_STRING);
 
 	if (strlen (string->str) == 4)
-		r->value.string = g_strdup ("");
-	else
-		r->value.string = string->str;
-	g_string_free (string, FALSE);
+		g_string_set_size (string, 0);
+	r->value.string = g_string_free (string, FALSE);
 
 	return r;
 }
