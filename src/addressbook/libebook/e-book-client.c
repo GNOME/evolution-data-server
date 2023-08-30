7fb1ac6fd66555f3cb0ffe54fcb37e0c915a18eb
Corentin Noël <corentin.noel@collabora.com>
2023-08-30 15:58:36 +0200

    M!127 - EBookClient: Port e_book_client_get_contact to GTask
    Closes: https://gitlab.gnome.org/GNOME/evolution-data-server/-/merge_requests/127

--- src/addressbook/libebook/e-book-client.c
+++ src/addressbook/libebook/e-book-client.c
@@ -3105,19 +3105,19 @@ e_book_client_remove_contacts_sync (EBookClient *client,
 
 /* Helper for e_book_client_get_contact() */
 static void
-book_client_get_contact_thread (GSimpleAsyncResult *simple,
-                                GObject *source_object,
+book_client_get_contact_thread (GTask *task,
+                                gpointer source_object,
+                                gpointer task_data,
                                 GCancellable *cancellable)
 {
-	AsyncContext *async_context;
 	GError *local_error = NULL;
-
-	async_context = g_simple_async_result_get_op_res_gpointer (simple);
+	const gchar *uid = task_data;
+	EContact *contact = NULL;
 
 	if (!e_book_client_get_contact_sync (
 		E_BOOK_CLIENT (source_object),
-		async_context->uid,
-		&async_context->contact,
+		uid,
+		&contact,
 		cancellable, &local_error)) {
 			if (!local_error)
 				local_error = g_error_new_literal (
@@ -3126,8 +3126,10 @@ book_client_get_contact_thread (GSimpleAsyncResult *simple,
 					_("Unknown error"));
 	}
 
-	if (local_error != NULL)
-		g_simple_async_result_take_error (simple, local_error);
+	if (!local_error)
+		g_task_return_pointer (task, g_steal_pointer (&contact), g_object_unref);
+	else
+		g_task_return_error (task, g_steal_pointer (&local_error));
 }
 
 /**
@@ -3138,7 +3140,7 @@ book_client_get_contact_thread (GSimpleAsyncResult *simple,
  * @callback: callback to call when a result is ready
  * @user_data: user data for the @callback
  *
- * Receive #EContact from the @client for the gived @uid.
+ * Receive #EContact from the @client for the given @uid.
  * The call is finished by e_book_client_get_contact_finish()
  * from the @callback.
  *
@@ -3151,29 +3153,19 @@ e_book_client_get_contact (EBookClient *client,
                            GAsyncReadyCallback callback,
                            gpointer user_data)
 {
-	GSimpleAsyncResult *simple;
-	AsyncContext *async_context;
+	GTask *task;
 
 	g_return_if_fail (E_IS_BOOK_CLIENT (client));
 	g_return_if_fail (uid != NULL);
 
-	async_context = g_slice_new0 (AsyncContext);
-	async_context->uid = g_strdup (uid);
+	task = g_task_new (client, cancellable, callback, user_data);
+	g_task_set_source_tag (task, e_book_client_get_contact);
+	g_task_set_check_cancellable (task, TRUE);
+	g_task_set_task_data (task, g_strdup (uid), g_free);
 
-	simple = g_simple_async_result_new (
-		G_OBJECT (client), callback, user_data,
-		e_book_client_get_contact);
+	g_task_run_in_thread (task, book_client_get_contact_thread);
 
-	g_simple_async_result_set_check_cancellable (simple, cancellable);
-
-	g_simple_async_result_set_op_res_gpointer (
-		simple, async_context, (GDestroyNotify) async_context_free);
-
-	g_simple_async_result_run_in_thread (
-		simple, book_client_get_contact_thread,
-		G_PRIORITY_DEFAULT, cancellable);
-
-	g_object_unref (simple);
+	g_object_unref (task);
 }
 
 /**
@@ -3197,26 +3189,19 @@ e_book_client_get_contact_finish (EBookClient *client,
                                   EContact **out_contact,
                                   GError **error)
 {
-	GSimpleAsyncResult *simple;
-	AsyncContext *async_context;
+	EContact *contact;
+	gboolean res;
 
-	g_return_val_if_fail (
-		g_simple_async_result_is_valid (
-		result, G_OBJECT (client),
-		e_book_client_get_contact), FALSE);
+	g_return_val_if_fail (g_task_is_valid (result, client), FALSE);
+	g_return_val_if_fail (g_async_result_is_tagged (result, e_book_client_get_contact), FALSE);
 
-	simple = G_SIMPLE_ASYNC_RESULT (result);
-	async_context = g_simple_async_result_get_op_res_gpointer (simple);
-
-	if (g_simple_async_result_propagate_error (simple, error))
-		return FALSE;
-
-	g_return_val_if_fail (async_context->contact != NULL, FALSE);
-
+	contact = g_task_propagate_pointer (G_TASK (result), error);
+	res = contact != NULL;
 	if (out_contact != NULL)
-		*out_contact = g_object_ref (async_context->contact);
+		*out_contact = g_steal_pointer (&contact);
 
-	return TRUE;
+	g_clear_object (&contact);
+	return res;
 }
 
 /**
