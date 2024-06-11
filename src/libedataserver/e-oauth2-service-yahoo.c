2c554828b73ec13397457340744f091798cc3fe2
Milan Crha <mcrha@redhat.com>
2024-06-11 13:39:09 +0200

    EOAuth2ServiceYahoo: Update redirect URI
    The wiki's page is too expensive to load. Replace it with a simple
    page with minimum distracting artifacts in it.

--- src/libedataserver/e-oauth2-service-yahoo.c
+++ src/libedataserver/e-oauth2-service-yahoo.c
@@ -134,7 +134,7 @@ static const gchar *
 eos_yahoo_get_redirect_uri (EOAuth2Service *service,
 			    ESource *source)
 {
-	return "https://gitlab.gnome.org/GNOME/evolution/-/wikis/YahooOAuth2/";
+	return "https://gnome.pages.gitlab.gnome.org/evolution/YahooOAuth2/";
 }
 
 static void
