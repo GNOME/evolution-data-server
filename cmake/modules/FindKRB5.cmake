40fd8116f8cc8f041707e76d200b8651b397f260
Dan Cîrnaț <dan@alt.md>
2025-01-07 14:05:50 +0000

    M!167 - build: Use Heimdal KRB5 implementation when found
    When Heimdal KRB5 implementation is identified, set up the proper CMake variables.
    
    This fixes building evolution-data-server with Heimdal KRB5
    
    Closes https://gitlab.gnome.org/GNOME/evolution-data-server/-/merge_requests/167

--- cmake/modules/FindKRB5.cmake
+++ cmake/modules/FindKRB5.cmake
@@ -34,6 +34,14 @@ if(KRB5_FOUND)
 		return()
 	endif(KRB5_VENDOR STREQUAL "MIT")
 
+	if(KRB5_VENDOR STREQUAL "Heimdal")
+		message(STATUS "Using Heimdal KRB5 implementation (found by pkg-config)")
+		set(WITH_KRB5 ON)
+		set(HAVE_KRB5 ON)
+		set(HAVE_HEIMDAL_KRB5 ON)
+		return()
+	endif(KRB5_VENDOR STREQUAL "Heimdal")
+
 	message(STATUS "Found Kerberos 5 with pkg-config, but unknown vendor '${KRB5_VENDOR}', continue with autodetection")
 endif()
 
