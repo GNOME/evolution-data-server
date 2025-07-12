c615e493a4da520aacbaac635e209a0020650d60
Corentin Noël <corentin.noel@collabora.com>
2025-07-12 23:20:19 +0200

    EBookContacts: Use g_object_notify_by_pspec when possible
    Use the GParamSpec directly to notify properties changes.

--- src/addressbook/libebook-contacts/e-source-backend-summary-setup.c
+++ src/addressbook/libebook-contacts/e-source-backend-summary-setup.c
@@ -54,8 +54,11 @@ struct _ESourceBackendSummarySetupPrivate {
 enum {
 	PROP_0,
 	PROP_SUMMARY_FIELDS,
-	PROP_INDEXED_FIELDS
+	PROP_INDEXED_FIELDS,
+	N_PROPS
 };
+
+static GParamSpec *properties[N_PROPS] = { NULL, };
 
 G_DEFINE_TYPE_WITH_PRIVATE (
 	ESourceBackendSummarySetup,
@@ -92,21 +95,21 @@ source_backend_summary_setup_set_literal_fields (ESourceBackendSummarySetup *ext
                                                  const gchar *literal_fields,
                                                  gint which)
 {
-	const gchar *property_name;
+	gint property_id;
 	gchar **target;
 
 	switch (which) {
 		case PROP_SUMMARY_FIELDS:
 			target = &(extension->priv->summary_fields);
-			property_name = "summary-fields";
+			property_id = PROP_SUMMARY_FIELDS;
 			break;
 		case PROP_INDEXED_FIELDS:
 			target = &(extension->priv->indexed_fields);
-			property_name = "indexed-fields";
+			property_id = PROP_INDEXED_FIELDS;
 			break;
 		default:
 			g_return_if_reached ();
-			break;
+			return;
 	}
 
 	g_mutex_lock (&extension->priv->property_lock);
@@ -121,7 +124,7 @@ source_backend_summary_setup_set_literal_fields (ESourceBackendSummarySetup *ext
 
 	g_mutex_unlock (&extension->priv->property_lock);
 
-	g_object_notify (G_OBJECT (extension), property_name);
+	g_object_notify_by_pspec (G_OBJECT (extension), properties[property_id]);
 }
 
 static void
@@ -192,34 +195,39 @@ e_source_backend_summary_setup_class_init (ESourceBackendSummarySetupClass *clas
 	extension_class = E_SOURCE_EXTENSION_CLASS (class);
 	extension_class->name = E_SOURCE_EXTENSION_BACKEND_SUMMARY_SETUP;
 
-	g_object_class_install_property (
-		object_class,
-		PROP_SUMMARY_FIELDS,
+	/**
+	 * ESourceBackendSummarySetup:summary-fields
+	 *
+	 * The list of quick reference summary fields
+	 **/
+	properties[PROP_SUMMARY_FIELDS] =
 		g_param_spec_string (
 			"summary-fields",
-			"Summary Fields",
-			"The list of quick reference summary fields",
+			NULL, NULL,
 			NULL,
 			G_PARAM_READWRITE |
 			G_PARAM_CONSTRUCT |
 			G_PARAM_EXPLICIT_NOTIFY |
 			G_PARAM_STATIC_STRINGS |
-			E_SOURCE_PARAM_SETTING));
+			E_SOURCE_PARAM_SETTING);
 
-	g_object_class_install_property (
-		object_class,
-		PROP_INDEXED_FIELDS,
+	/**
+	 * ESourceBackendSummarySetup:indexed-fields
+	 *
+	 * The list of summary fields which are to be given indexes in the underlying database
+	 **/
+	properties[PROP_INDEXED_FIELDS] =
 		g_param_spec_string (
 			"indexed-fields",
-			"Indexed Fields",
-			"The list of summary fields which are to be "
-			"given indexes in the underlying database",
+			NULL, NULL,
 			NULL,
 			G_PARAM_READWRITE |
 			G_PARAM_CONSTRUCT |
 			G_PARAM_EXPLICIT_NOTIFY |
 			G_PARAM_STATIC_STRINGS |
-			E_SOURCE_PARAM_SETTING));
+			E_SOURCE_PARAM_SETTING);
+
+	g_object_class_install_properties (object_class, N_PROPS, properties);
 }
 
 static void
