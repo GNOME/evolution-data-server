GNOME/evolution-data-server

commit 30ea003652910cbbe93cce33b03aed10a99fd969

M!159 - Add helper object to talk to intune (Microsoft OAuth2 broker service)
This patch adds infrastructure to communicate with a locally running
microsoft-identity-broker (Intune) via DBus to get a PRT SSO cookies.
This infrastructure is needed to implement support for the ms-oapxbc
oauth2.0 protocol extension. Support for that is needed to access
EWS endpoints behind "conditional access policies". At a later point
in time the added infrastructure might be moved to a standalone
library. Until this decision is made and we have broad distro
support, the relevant parts are duplicated here.

The code introduces a new dependency to libuuid.

Co-Developed-by: Milan Crha <mcrha@redhat.com>
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

Related to https://gitlab.gnome.org/GNOME/evolution-data-server/-/merge_requests/159

src/libedataserver/e-ms-oapxbc.c
@@ -1,0 +1,440 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * SPDX-FileCopyrightText: (C) 2024 Siemens AG
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+/**
+ * SECTION: e-ms-oapxbc
+ * @include: libedataserver/libedataserver.h
+ * @short_description: Interact with a locally running Microsoft OAuth2 broker service
+ *
+ * An #EMsOapxbc object provides methods to interact with a locally running Microsoft
+ * OAuth2 broker service to implement the OAuth2 ms-oapxbc extension. This extension
+ * defines how broker clients can interact with the Microsoft OAuth2 endpoints.
+ * The key concept hereby are the PRT SSO cookies, which are acquired from a locally running
+ * broker service and are injected into the login UI and token refresh requests.
+ *
+ * To get PRT SSO cookies, first call e_ms_oapxbc_get_accounts_sync() to get the
+ * users that are currently registered at the broker. Then, call
+ * e_ms_oapxbc_acquire_prt_sso_cookie_sync() with the account object that matches
+ * the user you want to get a PRT SSO cookie for. The PRT SSO cookies need to
+ * be injected either as cookie or as header into the login UI, as well as the
+ * token refresh requests. Note, that the PRT SSO cookies are short-lived with
+ * a minimal lifetime of 60 minutes.
+ *
+ * Since: 3.54
+ **/
+
+#include "evolution-data-server-config.h"
+
+#include <gio/gio.h>
+#include <glib/gi18n-lib.h>
+#include <json-glib/json-glib.h>
+#include <libsoup/soup.h>
+#include <uuid/uuid.h>
+
+#include "e-ms-oapxbc.h"
+
+#define DBUS_SERVICE_DBUS		"org.freedesktop.DBus"
+#define DBUS_PATH_DBUS			"/org/freedesktop/DBus"
+#define DBUS_START_REPLY_SUCCESS         1 /* Service was auto started */
+#define DBUS_START_REPLY_ALREADY_RUNNING 2 /* Service was already running */
+
+#define DBUS_BROKER_NAME "com.microsoft.identity.broker1"
+#define DBUS_BROKER_PATH "/com/microsoft/identity/broker1"
+#define AUTH_TYPE_OAUTH2 8
+
+struct _EMsOapxbc {
+	GObject parent_instance;
+
+	gchar client_id[UUID_STR_LEN];
+	gchar session_id[UUID_STR_LEN];
+	gchar *authority;
+	GDBusConnection *connection;
+};
+
+G_DEFINE_TYPE (EMsOapxbc, e_ms_oapxbc, G_TYPE_OBJECT)
+
+static void
+e_ms_oapxbc_finalize (GObject *object)
+{
+	EMsOapxbc *self = E_MS_OAPXBC (object);
+
+	g_clear_pointer (&self->authority, g_free);
+	g_clear_object (&self->connection);
+
+	G_OBJECT_CLASS (e_ms_oapxbc_parent_class)->finalize (object);
+}
+
+static void
+e_ms_oapxbc_class_init (EMsOapxbcClass *klass)
+{
+	GObjectClass *object_class;
+
+	object_class = G_OBJECT_CLASS (klass);
+	object_class->finalize = e_ms_oapxbc_finalize;
+}
+
+static void
+e_ms_oapxbc_init (EMsOapxbc *self)
+{
+	uuid_t session_id;
+
+	uuid_generate_random (session_id);
+	uuid_unparse_lower (session_id, self->session_id);
+}
+
+static gboolean
+ensure_broker_is_running_sync (EMsOapxbc *self,
+			       GCancellable *cancellable,
+			       GError **error)
+{
+	GDBusMessage *method_call_message;
+	GDBusMessage *method_reply_message;
+	GVariant *response;
+	guint response_state = 0;
+
+	method_call_message = g_dbus_message_new_method_call (DBUS_SERVICE_DBUS, DBUS_PATH_DBUS, NULL, "StartServiceByName");
+	g_dbus_message_set_body (method_call_message, g_variant_new("(su)", DBUS_BROKER_NAME, 0));
+	method_reply_message = g_dbus_connection_send_message_with_reply_sync (self->connection, method_call_message,
+		G_DBUS_SEND_MESSAGE_FLAGS_NONE, -1, NULL, cancellable, error);
+	g_object_unref (method_call_message);
+
+	if (method_reply_message == NULL) {
+		g_prefix_error (error, _("Failed to start broker (%s): "), DBUS_BROKER_NAME);
+		return FALSE;
+	}
+
+	response = g_dbus_message_get_body (method_reply_message);
+	g_variant_get (response, "(u)", &response_state);
+	g_object_unref (method_reply_message);
+
+	if (response_state != DBUS_START_REPLY_ALREADY_RUNNING &&
+	    response_state != DBUS_START_REPLY_SUCCESS) {
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+/**
+ * e_ms_oapxbc_new_sync:
+ * @client_id: a client ID of the broker client (UUID string).
+ * @authority: an authority URL of the OAuth2 service
+ * @cancellable: a #GCancellable
+ * @error: return location for a #GError, or %NULL
+ *
+ * Synchronously create a new #EMsOapxbc.
+ * This initiates the communication with a locally running Microsoft Identity broker service
+ * via DBus. In case the broker is not running, it is started. If no broker is registered,
+ * this function will fail (return %NULL).
+ *
+ * Returns: (nullable) (transfer full): a new #EMsOapxbc
+ *
+ * Since: 3.54
+ **/
+EMsOapxbc *
+e_ms_oapxbc_new_sync (const gchar *client_id,
+		      const gchar *authority,
+		      GCancellable *cancellable,
+		      GError **error)
+{
+	EMsOapxbc *self;
+
+	self = g_object_new (E_TYPE_MS_OAPXBC, NULL);
+
+	strncpy (self->client_id, client_id, UUID_STR_LEN - 1);
+	self->authority = g_strdup (authority);
+
+	self->connection = g_bus_get_sync (G_BUS_TYPE_SESSION, cancellable, error);
+
+	if (!self->connection) {
+		g_prefix_error (error, _("Failed to get seesion bus: "));
+		g_clear_object (&self);
+
+		return NULL;
+	}
+
+	if (!ensure_broker_is_running_sync (self, cancellable, error))
+		g_clear_object (&self);
+
+	return self;
+}
+
+/**
+ * e_ms_oapxbc_get_accounts_sync:
+ * @self: an #EMsOapxbc
+ * @cancellable: a #GCancellable
+ * @error: return location for a #GError, or %NULL
+ *
+ * Synchronously calls getAccounts() D-Bus method on the Microsoft
+ * OAuth2 broker service and returns the result as a #JsonObject.
+
+ * The #JsonObject contains the accounts that are currently registered at the broker,
+ * whereby the "accounts" node provides a #JsonArray of account entries. Note, that
+ * the availability of the types and entries needs to be checked by the caller before
+ * accessing them. The accounts entries can be inspected e.g. for the "username" and 
+ * "homeAccountId" fields. Then, one entry needs to be selected and passed as-is to
+ * e_ms_oapxbc_acquire_prt_sso_cookie_sync().
+ *
+ * Returns: (nullable) (transfer full): the accounts, or %NULL on error
+ *
+ * Since: 3.54
+ **/
+JsonObject *
+e_ms_oapxbc_get_accounts_sync (EMsOapxbc *self,
+			       GCancellable *cancellable,
+			       GError **error)
+{
+	GDBusMessage *method_call_message;
+	GDBusMessage *method_reply_message;
+	const gchar *response;
+	JsonBuilder *builder;
+	JsonGenerator *generator;
+	JsonParser *parser;
+	JsonNode *root;
+	JsonObject *accounts;
+	gchar *data;
+	gboolean parse_ok;
+
+	g_return_val_if_fail (E_IS_MS_OAPXBC (self), NULL);
+
+	builder = json_builder_new ();
+	json_builder_begin_object (builder);
+	json_builder_set_member_name (builder, "clientId");
+	json_builder_add_string_value (builder, self->client_id);
+	json_builder_set_member_name (builder, "redirectUri");
+	json_builder_add_string_value (builder, self->client_id);
+	json_builder_end_object (builder);
+	root = json_builder_get_root (builder);
+	g_object_unref (builder);
+
+	generator = json_generator_new ();
+	json_generator_set_root (generator, root);
+	data = json_generator_to_data (generator, NULL);
+	json_node_unref (root);
+	g_object_unref (generator);
+
+	method_call_message = g_dbus_message_new_method_call (
+		DBUS_BROKER_NAME, DBUS_BROKER_PATH, NULL, "getAccounts");
+	g_dbus_message_set_body (
+		method_call_message,
+		g_variant_new ("(sss)", "0.0", self->session_id, data));
+	method_reply_message = g_dbus_connection_send_message_with_reply_sync (
+		self->connection, method_call_message, G_DBUS_SEND_MESSAGE_FLAGS_NONE,
+		-1, NULL, cancellable, error);
+	g_free (data);
+	g_object_unref (method_call_message);
+
+	if (method_reply_message == NULL) {
+		g_prefix_error (error, _("Failed to call getAccounts: "));
+		return NULL;
+	}
+
+	response = g_dbus_message_get_arg0 (method_reply_message);
+
+	parser = json_parser_new ();
+	parse_ok = json_parser_load_from_data (parser, response, -1, error);
+	if (!parse_ok) {
+		g_prefix_error (error, _("Failed to parse getAccounts response: "));
+		g_clear_object (&parser);
+		g_clear_object (&method_reply_message);
+		return NULL;
+	}
+
+	root = json_parser_get_root (parser);
+	if (json_node_get_value_type (root) != JSON_TYPE_OBJECT) {
+		g_set_error (error, G_IO_ERROR, G_IO_ERROR_INVALID_DATA, _("Failed to parse getAccounts response: root is not an object"));
+		g_clear_object (&parser);
+		g_clear_object (&method_reply_message);
+		return NULL;
+	}
+
+	accounts = json_node_get_object (root);
+	json_object_ref (accounts);
+
+	g_object_unref (parser);
+	g_object_unref (method_reply_message);
+
+	return accounts;
+}
+
+static gchar *
+prepare_prt_sso_request_data (JsonObject *account,
+			      JsonObject *auth_params,
+			      const gchar *sso_url)
+{
+	JsonNode *params, *account_node, *auth_params_node, *sso_url_node;
+	JsonObject *params_obj;
+	JsonGenerator *gen;
+	gchar *data;
+
+	params = json_node_new (JSON_NODE_OBJECT);
+	params_obj = json_object_new ();
+	json_node_set_object (params, params_obj);
+	json_object_unref (params_obj);
+	account_node = json_node_new (JSON_NODE_OBJECT);
+	json_node_set_object (account_node, account);
+	auth_params_node = json_node_new (JSON_NODE_OBJECT);
+	json_node_set_object (auth_params_node, auth_params);
+	sso_url_node = json_node_new (JSON_NODE_VALUE);
+	json_node_set_string (sso_url_node, sso_url);
+
+	json_object_set_member (params_obj, "account", account_node);
+	json_object_set_member (params_obj, "authParameters", auth_params_node);
+	json_object_set_member (params_obj, "ssoUrl", sso_url_node);
+
+	gen = json_generator_new ();
+	json_generator_set_root (gen, params);
+	data = json_generator_to_data (gen, NULL);
+	g_object_unref (gen);
+	json_node_unref (params);
+
+	return data;
+}
+
+static JsonObject *
+prepare_prt_auth_params (EMsOapxbc *self,
+			 JsonObject *account,
+			 JsonArray *scopes,
+			 const gchar *redirect_uri)
+{
+	JsonNode *account_node, *scopes_node, *root;
+	JsonObject *auth_params;
+	JsonBuilder *builder;
+	gchar *use_redirect_uri = NULL;
+	const gchar *username;
+
+	account_node = json_node_new (JSON_NODE_OBJECT);
+	json_node_set_object (account_node, account);
+	scopes_node = json_node_new (JSON_NODE_ARRAY);
+	json_node_set_array (scopes_node, scopes);
+	username = json_object_get_string_member (account, "username");
+
+	if (!redirect_uri)
+		use_redirect_uri = g_strdup_printf ("%s/oauth2/nativeclient", self->authority);
+
+	builder = json_builder_new ();
+	json_builder_begin_object (builder);
+	json_builder_set_member_name (builder, "account");
+	json_builder_add_value (builder, account_node);
+	json_builder_set_member_name (builder, "authority");
+	json_builder_add_string_value (builder, self->authority);
+	json_builder_set_member_name (builder, "authorizationType");
+	json_builder_add_int_value (builder, AUTH_TYPE_OAUTH2);
+	json_builder_set_member_name (builder, "clientId");
+	json_builder_add_string_value (builder, self->client_id);
+	json_builder_set_member_name (builder, "redirectUri");
+	json_builder_add_string_value (builder, use_redirect_uri ? use_redirect_uri : redirect_uri);
+	json_builder_set_member_name (builder, "requestedScopes");
+	json_builder_add_value (builder, scopes_node);
+	json_builder_set_member_name (builder, "username");
+	json_builder_add_string_value (builder, username);
+	json_builder_end_object (builder);
+
+	root = json_builder_get_root (builder);
+	auth_params = json_node_get_object (root);
+	json_object_ref (auth_params);
+
+	g_object_unref (builder);
+	json_node_unref (root);
+	g_free (use_redirect_uri);
+
+	return auth_params;
+}
+
+/**
+ * e_ms_oapxbc_acquire_prt_sso_cookie_sync:
+ * @self: an #EMsOapxbc
+ * @account: an account returned from e_ms_oapxbc_get_accounts_sync()
+ * @sso_url: an SSO URL to acquire the PRT SSO cookie for.
+ * @scopes: array of scopes
+ * @redirect_uri: redirect URI
+ * @cancellable: a #GCancellable
+ * @error: return location for a #GError, or %NULL
+ *
+ * Synchronously calls acquirePrtSsoCookie() D-Bus method on the Microsoft
+ * OAuth2 broker service and converts the result into a new #SoupCookie.
+ * The account object needs to be taken from the accounts list that is returned by
+ * e_ms_oapxbc_get_accounts_sync(). The SSO URL is the OAuth2 authentication endpoint.
+ * The scopes are the requested scopes for the OAuth2 service (usually only
+ * https://graph.microsoft.com/.default). The redirect URI is the OAuth2 service
+ * redirect URI.
+ *
+ * Returns: (nullable) (transfer full): an acquired cookie, or %NULL on error
+ *
+ * Since: 3.54
+ **/
+SoupCookie *
+e_ms_oapxbc_acquire_prt_sso_cookie_sync (EMsOapxbc *self,
+					 JsonObject *account,
+					 const gchar *sso_url,
+					 JsonArray *scopes,
+					 const gchar *redirect_uri,
+					 GCancellable *cancellable,
+					 GError **error)
+{
+	JsonNode *root;
+	JsonObject *auth_params, *json_cookie;
+	JsonParser *parser;
+	GDBusMessage *method_call_message;
+	GDBusMessage *method_reply_message;
+	SoupCookie *soup_cookie;
+	gchar *data;
+	const gchar *response;
+	gboolean parse_ok;
+
+	g_return_val_if_fail (E_IS_MS_OAPXBC (self), NULL);
+
+	auth_params = prepare_prt_auth_params (self, account, scopes, redirect_uri);
+	data = prepare_prt_sso_request_data (account, auth_params, sso_url);
+	json_object_unref (auth_params);
+
+	method_call_message = g_dbus_message_new_method_call (DBUS_BROKER_NAME, DBUS_BROKER_PATH, NULL, "acquirePrtSsoCookie");
+	g_dbus_message_set_body (method_call_message, g_variant_new ("(sss)", "0.0", self->session_id, data));
+	method_reply_message = g_dbus_connection_send_message_with_reply_sync (
+		self->connection, method_call_message,
+		G_DBUS_SEND_MESSAGE_FLAGS_NONE, -1, NULL,
+		cancellable, error);
+	g_free (data);
+	g_object_unref (method_call_message);
+
+	if (method_reply_message == NULL) {
+		g_prefix_error (error, _("Failed to acquire PRT SSO cookie: "));
+		return NULL;
+	}
+
+	response = g_dbus_message_get_arg0 (method_reply_message);
+	parser = json_parser_new ();
+	parse_ok = json_parser_load_from_data (parser, response, -1, error);
+	if (!parse_ok) {
+		g_prefix_error (error, _("Failed to parse acquirePrtSsoCookie response: "));
+		g_clear_object (&method_reply_message);
+		g_clear_object (&parser);
+		return NULL;
+	}
+	root = json_parser_get_root (parser);
+	if (json_node_get_value_type (root) != JSON_TYPE_OBJECT) {
+		g_set_error (error, G_IO_ERROR, G_IO_ERROR_INVALID_DATA, _("Failed to parse acquirePrtSsoCookie response: root is not an object"));
+		g_clear_object (&method_reply_message);
+		g_clear_object (&parser);
+		return NULL;
+	}
+
+	json_cookie = json_node_get_object (root);
+
+	soup_cookie = soup_cookie_new (
+		json_object_get_string_member (json_cookie, "cookieName"),
+		json_object_get_string_member (json_cookie, "cookieContent"),
+		/* [ms-oapxbc] is only supported on Microsoft Entra ID */
+		"login.microsoftonline.com",
+		"/", -1);
+	soup_cookie_set_secure (soup_cookie, TRUE);
+	soup_cookie_set_http_only (soup_cookie, TRUE);
+
+	g_object_unref (parser);
+	g_object_unref (method_reply_message);
+
+	return soup_cookie;
+}

CMakeLists.txt
@@ -120,6 +120,7 @@ set(libical_glib_minimum_version 3.0.7)
 set(libsecret_minimum_version 0.5)
 set(libxml_minimum_version 2.0.0)
 set(sqlite_minimum_version 3.7.17)
+set(uuid_minimum_version 2.0)
 
 # Optional Packages
 set(goa_minimum_version 3.8)
@@ -420,6 +421,7 @@ endif(ENABLE_GTK4)
 # **************************************************************
 
 pkg_check_modules(JSON_GLIB REQUIRED json-glib-1.0>=${json_glib_minimum_version})
+pkg_check_modules(UUID REQUIRED uuid>=${uuid_minimum_version})
 
 if(ENABLE_GTK)
 	add_printable_option(ENABLE_OAUTH2_WEBKITGTK "Enable WebKitGTK gtk3 for built-in OAuth2 authentications" ON)

docs/reference/evolution-data-server/evolution-data-server-docs.sgml.in
@@ -251,6 +251,7 @@
       <xi:include href="xml/e-gdata-session.xml"/>
       <xi:include href="xml/e-json-utils.xml"/>
       <xi:include href="xml/e-memory.xml"/>
+      <xi:include href="xml/e-ms-oapxbc.xml"/>
       <xi:include href="xml/e-named-parameters.xml"/>
       <xi:include href="xml/e-network-monitor.xml"/>
       <xi:include href="xml/e-operation-pool.xml"/>

po/POTFILES.in
@@ -195,6 +195,7 @@ src/libebackend/e-user-prompter-server.c
 src/libedataserver/e-categories.c
 src/libedataserver/e-client.c
 src/libedataserver/e-gdata-session.c
+src/libedataserver/e-ms-oapxbc.c
 src/libedataserver/e-oauth2-service.c
 src/libedataserver/e-oauth2-service-google.c
 src/libedataserver/e-oauth2-service-outlook.c

src/libedataserver/CMakeLists.txt
@@ -65,6 +65,7 @@ set(SOURCES
 	e-list-iterator.c
 	e-memory.c
 	e-module.c
+	e-ms-oapxbc.c
 	e-named-parameters.c
 	e-network-monitor.c
 	e-oauth2-service.c
@@ -158,6 +159,7 @@ set(HEADERS
 	e-list-iterator.h
 	e-memory.h
 	e-module.h
+	e-ms-oapxbc.h
 	e-named-parameters.h
 	e-network-monitor.h
 	e-oauth2-service.h
@@ -261,6 +263,7 @@ target_compile_options(edataserver PUBLIC
 	${GIO_UNIX_CFLAGS}
 	${ICU_CFLAGS}
 	${JSON_GLIB_CFLAGS}
+	${UUID_CFLAGS}
 )
 
 target_include_directories(edataserver PUBLIC
@@ -275,6 +278,7 @@ target_include_directories(edataserver PUBLIC
 	${GIO_UNIX_INCLUDE_DIRS}
 	${ICU_INCLUDE_DIRS}
 	${JSON_GLIB_INCLUDE_DIRS}
+	${UUID_INCLUDE_DIRS}
 )
 
 target_link_libraries(edataserver
@@ -283,6 +287,7 @@ target_link_libraries(edataserver
 	${GIO_UNIX_LDFLAGS}
 	${ICU_LDFLAGS}
 	${JSON_GLIB_LDFLAGS}
+	${UUID_LDFLAGS}
 )
 
 install(TARGETS edataserver

src/libedataserver/e-ms-oapxbc.h
@@ -1,0 +1,43 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * SPDX-FileCopyrightText: (C) 2024 Siemens AG
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#if !defined (__LIBEDATASERVER_H_INSIDE__) && !defined (LIBEDATASERVER_COMPILATION)
+#error "Only <libedataserver/libedataserver.h> should be included directly."
+#endif
+
+#include <json-glib/json-glib.h>
+#include <libsoup/soup.h>
+
+#ifndef E_MS_OAPXBC_H
+#define E_MS_OAPXBC_H
+
+G_BEGIN_DECLS
+
+#define E_TYPE_MS_OAPXBC e_ms_oapxbc_get_type ()
+G_DECLARE_FINAL_TYPE (EMsOapxbc, e_ms_oapxbc, E, MS_OAPXBC, GObject)
+
+EMsOapxbc *	e_ms_oapxbc_new_sync	(const gchar *client_id,
+					 const gchar *authority,
+					 GCancellable *cancellable,
+					 GError **error);
+
+JsonObject *	e_ms_oapxbc_get_accounts_sync
+					(EMsOapxbc *self,
+					 GCancellable *cancellable,
+					 GError **error);
+
+SoupCookie *	e_ms_oapxbc_acquire_prt_sso_cookie_sync
+					(EMsOapxbc *self,
+					 JsonObject *account,
+					 const gchar *sso_url,
+					 JsonArray *scopes,
+					 const gchar *redirect_uri,
+					 GCancellable *cancellable,
+					 GError **error);
+
+G_END_DECLS
+
+#endif /* E_MS_OAPXBC_H */

src/libedataserver/libedataserver.h
@@ -39,6 +39,7 @@
 #include <libedataserver/e-list.h>
 #include <libedataserver/e-memory.h>
 #include <libedataserver/e-module.h>
+#include <libedataserver/e-ms-oapxbc.h>
 #include <libedataserver/e-named-parameters.h>
 #include <libedataserver/e-network-monitor.h>
 #include <libedataserver/e-oauth2-service.h>

commit e14416c488167bdcc8f425ae4cb6d045ea81976d

e-ms-oapxbc: Fix error handling of broker response
The code expected a particular layout of the JSON object that is
returned by the broker. As broker versions > 2.0.1 use a different
layout, the parser crashes. We fix this by hardening the parser against
unexpected data by checking if the objects we are interested in are
actually contained in the response.

Xref: !216
Fixes: 30ea00365 ("M!159 - Add helper object to talk to intune ...")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Part-of: <https://gitlab.gnome.org/GNOME/evolution-data-server/-/merge_requests/216>

src/libedataserver/e-ms-oapxbc.c
@@ -328,6 +328,8 @@ e_ms_oapxbc_acquire_prt_sso_cookie_sync (EMsOapxbc *self,
 	JsonNode *root;
 	JsonObject *auth_params, *json_cookie;
 	JsonParser *parser;
+	const gchar *cookie_name;
+	const gchar *cookie_content;
 	SoupCookie *soup_cookie = NULL;
 	gchar *data;
 	gchar *response = NULL;
@@ -363,13 +365,30 @@ e_ms_oapxbc_acquire_prt_sso_cookie_sync (EMsOapxbc *self,
 	}
 
 	json_cookie = json_node_get_object (root);
+	if (!json_object_has_member (json_cookie, "cookieName") ||
+	    !json_object_has_member (json_cookie, "cookieContent")) {
+		g_set_error (error, G_IO_ERROR, G_IO_ERROR_INVALID_DATA, _("Failed to parse acquirePrtSsoCookie response: invalid PrtSsoCookie data"));
+		goto error;
+	}
+
+	cookie_name = json_object_get_string_member (json_cookie, "cookieName");
+	cookie_content = json_object_get_string_member (json_cookie, "cookieContent");
+	if (!cookie_name || !cookie_content) {
+		g_set_error (error, G_IO_ERROR, G_IO_ERROR_INVALID_DATA, _("Failed to parse acquirePrtSsoCookie response: invalid PrtSsoCookie data"));
+		goto error;
+	}
 
 	soup_cookie = soup_cookie_new (
-		json_object_get_string_member (json_cookie, "cookieName"),
-		json_object_get_string_member (json_cookie, "cookieContent"),
+		cookie_name,
+		cookie_content,
 		/* [ms-oapxbc] is only supported on Microsoft Entra ID */
 		"login.microsoftonline.com",
 		"/", -1);
+	if (!soup_cookie) {
+		g_set_error (error, G_IO_ERROR, G_IO_ERROR_INVALID_DATA, _("Failed to create SOAP cookie"));
+		goto error;
+	}
+
 	soup_cookie_set_secure (soup_cookie, TRUE);
 	soup_cookie_set_http_only (soup_cookie, TRUE);
 